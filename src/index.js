e965091ceada12e2f245bbbcd9ec0fcc71e59d6a
Mona Lisa <halvor98@outlook.com>
2018-05-10 13:22:08 +0200

    Removed bootstrap

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './components/App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap';
 import './styles/app.scss';
 
 ReactDOM.render(<App />, document.getElementById('app'));
