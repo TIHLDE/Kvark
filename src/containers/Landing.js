5996d850aff809abfc1856d2e7b0c742f84ed2d0
Mona Lisa <halvor98@outlook.com>
2018-06-10 19:55:47 +0200

    Changed lint rules

--- src/containers/Landing.js
+++ src/containers/Landing.js
@@ -1,9 +1,5 @@
 import React, {Component, Fragment} from 'react';
-import Button from '../components/Button';
 
-/**
- *
- */
 export default class Landing extends Component {
     render() {
         return (
