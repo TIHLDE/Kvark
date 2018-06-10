1ecf16ad9f11d80c23dd2c1bd05ba1f1ace42589
Anders Hallem Iversen <anderhi@stud.ntnu.no>
2018-06-10 17:19:10 +0200

    The time will now update automatically

--- src/components/Time.js
+++ src/components/Time.js
@@ -3,9 +3,24 @@ import React, { Component } from 'react';
 
 export default class extends Component {
 
+    state = {
+        time : Date.now()
+    }
+
+    componentWillMount(){
+        this.updateTime();
+    }
+
+    updateTime = () => {
+        setTimeout(() => {
+            this.setState({time: Date.now()});
+            this.updateTime();
+        },1000);
+    }
+
     render() {
         return (
-            <h1>Dette er tiden: { Date.now() }</h1>
+            <h1>Dette er tiden: {this.state.time}</h1>
         );
     }
 
