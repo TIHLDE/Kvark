3320940473e8371bdd4526fce65e3a494de6c674
Anders Hallem Iversen <anderhi@stud.ntnu.no>
2018-06-11 20:27:55 +0200

    Added a Loading (spinner) component

---
+++ src/components/Loading/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './Loading.css';
+
+export default class Loading extends Component {
+    render() {
+        return (
+            <div className="Loading">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        );
+    }
+}
--- src/components/Progress/index.js
+++ src/components/Progress/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 //  CSS-imports
@@ -9,63 +9,62 @@ export default class Progress extends Component {
     constructor() {
         super();
         this.container = React.createRef();
-        this.barWidth = 400; //The width of the entire bar
+        this.barWidth = 400; // The width of the entire bar
     }
 
     componentDidMount() {
-        //Gets the width of the bar
+        // Gets the width of the bar
         this.barWidth = this.container.current.offsetWidth;
     }
 
-
-
     render() {
-        //Check progress-type
-        let progressStyle = "Progress";
+        // Check progress-type
+        let progressStyle = 'Progress';
         if (this.props.circular) {
-            progressStyle += " Circular"; //Line progress
+            progressStyle += ' Circular'; // Line progress
         }
         else {
-            progressStyle += " Line"; //Circular progress
+            progressStyle += ' Line'; // Circular progress
         }
 
-        //Initializes the color of the bar
-        let progressColor = "Blue";
-        if (this.props.color === "Red") {
-            progressColor = "Red";
+        // Initializes the color of the bar
+        let progressColor = 'Blue';
+        if (this.props.color === 'Red') {
+            progressColor = 'Red';
         }
-        else if(this.props.color === "Yellow"){
-            progressColor = "Yellow";
+        else if (this.props.color === 'Yellow') {
+            progressColor = 'Yellow';
         }
-        else if(this.props.colr === "Green"){
-            progressColor = "Green";
+        else if (this.props.color === 'Green') {
+            progressColor = 'Green';
         }
 
-        //Making sure the progress value is between 0 and 100
+        // Making sure the progress value is between 0 and 100
         const progressValue = (this.props.progress > 100) ? 100 :
             (this.props.progress < 0) ? 0 : this.props.progress;
 
-        //Styling for progressBar based on the progress
+        // Styling for progressBar based on the progress
         const progressWidth = this.barWidth * progressValue / 100;
         const progressBarStyle = {
-            width: progressWidth
-        }
+            width: progressWidth,
+        };
 
         return (
             <div className={progressStyle} ref={this.container}>
                 <div
                     className={progressColor}
-                    id="progressBar"
+                    id='progressBar'
                     style={progressBarStyle}>
                 </div>
-                <p id="progressText">{progressValue}%</p>
+                <p id='progressText'>{progressValue}%</p>
             </div>
-        )
+        );
     }
 }
 
-//Validates the type of the props
+// Validates the type of the props
 Progress.propTypes = {
     progress: PropTypes.number,
-    progressBar: PropTypes.bool
-}
+    circular: PropTypes.bool,
+    color: PropTypes.string,
+};
