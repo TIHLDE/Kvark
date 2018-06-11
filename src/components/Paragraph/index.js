04ebe86cce482da11728c4990490fc29113e14b1
Sveinung Øverland <sveinungg.overlandd@gmail.com>
2018-06-11 22:38:34 +0200

    Paragraph component

--- src/components/Paragraph/index.js
+++ src/components/Paragraph/index.js
@@ -1,7 +1,27 @@
 import Component from 'react';
 
 export default class Paragraph extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            style : {
+                width: props.width || 'auto',
+                height: props.height || 'auto',
+                overflow: props.overflow || 'hidden',
+                fontSize: props.fontSize || 'inherit'
+            },
+            text: props.text || 'Tekst mangler'
+        }
+    }
+
     render() {
-        return null;
+
+
+        return (
+            <p style={this.state.style}>
+                {props.text}
+            </p>
+        );
     }
 }
