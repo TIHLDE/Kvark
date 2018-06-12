6455ec21ee332be3281590db2f9b1c7d75b06047
Anders Hallem Iversen <anderhi@stud.ntnu.no>
2018-06-12 23:13:13 +0200

    Fixed some issues with the footer

--- src/containers/Landing.js
+++ src/containers/Landing.js
@@ -1,12 +1,10 @@
 import React, {Component, Fragment} from 'react';
-import Footer from '../components/Footer';
 
 export default class Landing extends Component {
     render() {
         return (
             <Fragment>
                 <h1>Hello Students!</h1>
-               <Footer/>
             </Fragment>
         );
     }
