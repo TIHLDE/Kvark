7ccc012912511d7c9c7dec9ae82755d1ddc23501
Jørgen Bele <jorgerei@stud.ntnu.no>
2018-06-13 12:27:35 +0200

    Created initial Header component.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,29 @@
-import Component from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 export default class Header extends Component {
     render() {
-        return null;
+        return (
+            <header className={this.props.className}>
+                <div className={this.props.textClassName}>
+                    {this.props.text}
+                </div>
+                {this.props.logo}
+                {this.props.children}
+            </header>
+        );
     }
 }
+
+Header.defaultProps = {
+    className: 'app-header',
+    textClassName: 'app-header',
+};
+
+Header.propTypes = {
+    className: PropTypes.string,
+    textClassName: PropTypes.string,
+    text: PropTypes.string,
+    logo: PropTypes.any,
+    children: PropTypes.any,
+};
