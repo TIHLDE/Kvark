bc1be1c0890231d102ae284bbd6c8d5766c41fa1
Jørgen Bele <jorgerei@stud.ntnu.no>
2018-06-13 12:28:04 +0200

    Created initial Picture component.

--- src/components/Picture/index.js
+++ src/components/Picture/index.js
@@ -1,7 +1,44 @@
-import Component from 'react';
+import React, {Fragment, Component} from 'react';
+import PropTypes from 'prop-types';
+
+import './Picture.css';
 
 export default class Picture extends Component {
     render() {
-        return null;
+        let className = this.props.className || '';
+        switch (this.props.type) {
+            case 'circular':
+                className += ' Circular';
+                break;
+            case 'normal':
+            default:
+                className += ' Normal';
+                break;
+        }
+
+        return (
+            <Fragment>
+                <img src={this.props.src}
+                    alt={this.props.alt}
+                    className={className}
+                    width={this.props.width}
+                    height={this.props.height}
+                />
+            </Fragment>
+        );
     }
 }
+
+Picture.defaultProps = {
+    type: 'normal',
+};
+
+Picture.propTypes = {
+    src: PropTypes.string.isRequired,
+    alt: PropTypes.string,
+    type: PropTypes.oneOf(['circular', 'normal', 'square']),
+    className: PropTypes.string,
+    width: PropTypes.number,
+    height: PropTypes.number,
+};
+
