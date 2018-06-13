f1bf2fdbd92fe668e0b4fdc5f9dec9e2b0b7bc21
Jørgen Bele <jorgerei@stud.ntnu.no>
2018-06-13 18:31:51 +0200

    Refactored to remove React warnings.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -17,15 +17,15 @@ export default class Footer extends Component {
         ];
 
         const icons = [
-            <a href="https://www.facebook.com"><img src={facebookIcon} alt = "Facebook Icon" width='40px'/></a>,
-            <a href="https://www.twitter.com"><img src={twitterIcon} alt = "Twitter Icon" width='40px'/></a>,
-            <a href="https://www.instagram.com"><img src={instagramIcon} alt = "Instagram Icon" width='40px'/></a>,
+            {link: 'https://www.facebook.com', iconSrc: facebookIcon, iconAlt: 'Facebook Icon'},
+            {link: 'https://www.twitter.com', iconSrc: twitterIcon, iconAlt: 'Twitter Icon'},
+            {link: 'https://www.instagram.com', iconSrc: instagramIcon, iconAlt: 'Instagram Icon'},
         ];
 
         return (
             <footer className="Footer">
                 <div id="tihldeIcon">
-                    <img src={tihldeImage} alt ="Tihlde Logo" width='100%'/>
+                    <img src={tihldeImage} alt="Tihlde Logo" width='100%' />
                 </div>
                 <div id="contactDiv">
                     <ul>
@@ -34,12 +34,18 @@ export default class Footer extends Component {
                 </div>
                 <div id="inputing">
                     <p>Enter your email and be the first to know when arrangement happens!</p>
-                    <input placeholder="Join us today!"/>
+                    <input placeholder="Join us today!" />
                 </div>
                 <div id="copyRightDiv">
                     <div id="iconDivs">
                         <ul>
-                            {icons.map((value, index) => <li className='Icons' key={index}>{value}</li>)}
+                            {icons.map((value, index) =>
+                                <li className='Icons' key={index}>{
+                                    <a href={value.link}>
+                                        <img src={value.iconSrc} alt={value.iconAlt} width='40px' />
+                                    </a>
+                                }</li>)
+                            }
                         </ul>
                     </div>
                 </div>
