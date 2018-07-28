a31f1df7c55c474257277dd91e8ce43ad43c2fcf
Jørgen Bele Reinfjell <jorgen.bele@gmail.com>
2018-07-28 17:29:28 +0200

    Fixed mistake in parameter list.

--- src/api/api.js
+++ src/api/api.js
@@ -3,6 +3,6 @@ import {IRequest} from './HttpHandler';
 export default {
     // Returns a set of items/widgets to display in the LandingPage grid
     getGridItems: () => {
-        return new IRequest('GET', 'items/', false);
+        return new IRequest('GET', 'items/', undefined, false);
     }
 }
