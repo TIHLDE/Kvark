8fb203f148b20c26c547d4e89c01fb2c9adb7e9b
Jørgen Bele Reinfjell <jorgen.bele@gmail.com>
2018-07-29 02:15:36 +0200

    Fixed (again) arguments of `api.js`.
    The `false` argument was passed as `data` instead of the intended
    `withAuth`.

--- src/api/api.js
+++ src/api/api.js
@@ -7,9 +7,9 @@ import {IRequest} from './HttpHandler';
 export default {
     // Returns a set of items/widgets to display in the LandingPage grid
     getGridItems: () => {
-        return new IRequest('GET', 'items/', false);
+        return new IRequest('GET', 'items/', undefined, false);
     },
     getNewsItem: (id) => {
-        return new IRequest('GET', 'news/'.concat(id, '/'), false);
+        return new IRequest('GET', 'news/'.concat(id, '/'), undefined, false);
     },
 };
