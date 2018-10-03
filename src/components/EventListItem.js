69b368dee887a8cc967d7fcedc114b5c06ee7754
william <willia2210@hotmail.no>
2018-10-03 11:01:25 +0200

    made more like requested

--- src/components/EventListItem.js
+++ src/components/EventListItem.js
@@ -13,33 +13,74 @@ import Divider from '@material-ui/core/Divider';
 import Button from '@material-ui/core/Button';
 import CardActionArea from '@material-ui/core/CardActionArea';
 
+// Icons
 import TIHLDE from '../assets/img/tihlde_image.png';
-import Time from '../assets/icons/twotone-access_time-24px.svg';
-import Calendar from '../assets/icons/twotone-calendar_today-24px.svg';
-import Location from '../assets/icons/twotone-location_on-24px.svg';
+import Time from '@material-ui/icons/AccessTime';
+import Calendar from '@material-ui/icons/CalendarToday';
+import Location from '@material-ui/icons/LocationOn';
 
+
+const DESKTOP_HEIGHT = 112;
+const DESKTOP_WIDTH = DESKTOP_HEIGHT +70;
 
 const styles = {
     root: {
         width: 'auto',
-        height: 'auto',
+        height: DESKTOP_HEIGHT,
         fontFamily:'arial',
         cursor: 'pointer',
-    },
-    image:{
-        height: '100px',
-        width:'100px',
-        borderStyle:'inset',
-        borderWidth:'2px',
-    },
-    imageContainer:{
-        padding:'40px',
+
         '@media only screen and (max-width: 800px)': {
-            maxWidth: 'none',
+            height: 'auto',
         }
     },
+    image:{
+        height: DESKTOP_HEIGHT,
+        width: DESKTOP_WIDTH,
+        maxWidth: DESKTOP_WIDTH,
+        objectFit: 'cover',
+        '@media only screen and (max-width: 800px)': {
+            width: '100%',
+            maxWidth: 'none',
+            height: 'auto',
+            maxHeight: 300,
+        }
+    },
+    imageContainer:{
+        paddingRight: 10,
+        minWidth: DESKTOP_WIDTH,
+        '@media only screen and (max-width: 800px)': {
+            maxWidth: '100%',
+            minWidth: '100%',
+            paddingRight: 0,
+        }
+    },
+    content: {
+        maxWidth: '100%',
+        '@media only screen and (max-width: 800px)': {
+            padding: 10,
+        },
+    },
     padding: {
-        padding: 20
+        padding: '20px 20px 10px 0',
+
+        '@media only screen and (max-width: 800px)': {
+            padding: 10,
+        },
+    },
+    smallPadding: {
+        paddingRight: 20,
+        maxWidth: 150,
+        paddingBottom: 10,
+
+        '@media only screen and (max-width: 800px)': {
+            paddingLeft: 10
+        },
+
+    },
+    rightPadding: {
+        paddingLeft: 10,
+        color: 'rgba(0,0,0,0.4)',
     },
     direction:{
         flexDirection:'row',
@@ -51,7 +92,12 @@ const styles = {
         marginLeft:'auto'
     },
     holder:{
-        paddingRight:50
+        padding: '10px 10px 10px 0',
+    },
+    buttonBase: {
+        padding: 0,
+        margin: 0,
+
     }
 };
 
@@ -64,29 +110,29 @@ class EventListItem extends Component {
         let start = moment(data.start, ['YYYY-MM-DD HH:mm'], "nb");
 
         return (
-            <CardActionArea>
-            <Paper className={classes.root} onClick={this.props.onClick}>
+            <CardActionArea classes={{root: classes.buttonBase}}>
+            <Paper className={classes.root} onClick={this.props.onClick} square>
                 <Grid container className={classes.direction} wrap='nowrap'>
                     <div className={classes.imageContainer}>
                         <img className={classes.image} alt="complex" src={image} />
                     </div>
-                    <Grid  container direction="column"  >
+                    <Grid className={classes.content} container direction="column"  >
                         <Grid className={classes.padding} item >
-                            <Typography variant="display1"> {data.title} </Typography>
+                            <Typography variant="headline"> {data.title} </Typography>
                         </Grid>
 
-                        <Grid container direction="row" alignItems='center' className={classes.holder}>
-                            <Grid item className={classes.padding} >
-                                <img src={Calendar}/>
-                                <Typography >{start.format('DD MM YYYY')} </Typography>
+                        <Grid container direction="row" alignItems='center' className={classes.holder} wrap='wrap'>
+                            <Grid container direction="row" alignItems='center' className={classes.smallPadding} wrap='nowrap'>
+                                <Calendar/>
+                                <Typography className={classes.rightPadding} noWrap>{start.format('DD-MM-YYYY')} </Typography>
                             </Grid>
-                            <Grid item className={classes.padding} >
-                                <img src={Time}/>
-                                <Typography> {start.format('HH:mm')}</Typography>
+                            <Grid container direction="row" alignItems='center' className={classes.smallPadding} wrap='nowrap'>
+                                <Time/>
+                                <Typography className={classes.rightPadding}> {start.format('HH:mm')}</Typography>
                             </Grid>
-                            <Grid item className={classes.padding} >
-                                <img src={Location}/>
-                                <Typography >{data.location}</Typography>
+                            <Grid container direction="row" alignItems='center' className={classes.smallPadding} wrap='nowrap'>
+                                <Location/>
+                                <Typography className={classes.rightPadding}>{data.location}</Typography>
                             </Grid>
                         </Grid>
                     </Grid>
