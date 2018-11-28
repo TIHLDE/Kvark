1aaca9e64b01c425aa08d25b3963d9fc8b72ce13
william <willia2210@hotmail.no>
2018-11-28 20:28:20 +0100

    added text

--- src/text/CompaniesText.js
+++ src/text/CompaniesText.js
@@ -2,16 +2,20 @@ export default {
     header: 'Meld interesse',
     bannnerTitle: 'Bedrifter',
     bannerPicture: 'https://3qrt0f3cy3lx35vbo81iuq7b-wpengine.netdna-ssl.com/wp-content/uploads/2017/05/GlobalShares-Software-Solutions-bg.jpg',
-    cardInfo: 'TIHLDE (Trondheim IngeniørHøgskoles Linjeforening for Dannede EDBere) er linjeforeningen for bachelorstudiene Dataingeniør, Drift av datasystemer og IT-støttet bedriftsutvikling, samt masterstudiet IKT-basert samhandling ved AIT, IDI, NTNU på Kalvskinnet.',
+    cardInfo: 'Alle studiene er i stor grad praktisk rettet, noe som skal gjøre studentene i stand til å gå rett ut i jobb etter endt utdannelse.',
     undergrupper: 'Undergrupper',
     studier: 'Studier',
-    data: 'TIHLDE (Trondheim IngeniørHøgskoles Linjeforening for Dannede EDBere) er linjeforeningen for bachelorstudiene Dataingeniør, Drift av datasystemer og IT-støttet bedriftsutvikling, samt masterstudiet IKT-basert samhandling ved AIT, IDI, NTNU på Kalvskinnet.',
-    drift: 'TIHLDE (Trondheim IngeniørHøgskoles Linjeforening for Dannede EDBere) er linjeforeningen for bachelorstudiene Dataingeniør, Drift av datasystemer og IT-støttet bedriftsutvikling, samt masterstudiet IKT-basert samhandling ved AIT, IDI, NTNU på Kalvskinnet.',
-    IKT: 'TIHLDE (Trondheim IngeniørHøgskoles Linjeforening for Dannede EDBere) er linjeforeningen for bachelorstudiene Dataingeniør, Drift av datasystemer og IT-støttet bedriftsutvikling, samt masterstudiet IKT-basert samhandling ved AIT, IDI, NTNU på Kalvskinnet.',
-    support: 'TIHLDE (Trondheim IngeniørHøgskoles Linjeforening for Dannede EDBere) er linjeforeningen for bachelorstudiene Dataingeniør, Drift av datasystemer og IT-støttet bedriftsutvikling, samt masterstudiet IKT-basert samhandling ved AIT, IDI, NTNU på Kalvskinnet.',
-    jobbannonser: 'TIHLDE (Trondheim IngeniørHøgskoles Linjeforening for Dannede EDBere) er linjeforeningen for bachelorstudiene Dataingeniør, Drift av datasystemer og IT-støttet bedriftsutvikling, samt masterstudiet IKT-basert samhandling ved AIT, IDI, NTNU på Kalvskinnet.',
+    data: 'Dataingeniør-studiet kombinerer det beste fra de spesialiserte informatikkutdanningene og de tradisjonelle ingeniørutdanningene.\n' +
+        'Det legger mye vekt på praktisk utvikling av systemer og programmer, og studentene får et godt grunnlag i datateknikk, matematikk og teknisk-naturvitenskapelige fag, samt varig og verdifull kompetanse om hvordan datateknikk kan benyttes.',
+    drift: 'Dette bachelorstudiet setter fokus på den drift-tekniske IKT-kompetansen bedrifter etterspør. Studentene lærer planleggingsprosesser og oppsett av virtuelle maskiner med bruk av teknologier som VMWare og HyperV. Videre temaer i studiet er Linux, Windows Server, “Cloud Computing” og overvåkning og sikkerhet i digital infrastruktur.',
+    IKT: 'Digital samhandling er et veletablert forskningsområde som tar for seg hvordan utøvelse og koordinering av samarbeidsaktiviteter kan støttes ved hjelp av ulike IKT-systemer. Studentene ved denne 2 årige masteren er i stand til å samhandle effektivt i forskjellige tverrfaglige problemløsningsprosesser.',
+    support: 'IT-støttet bedriftsutvikling (ITBU) kombinerer IT, økonomi og ledelse for å skape forretningsutviklere med tverrfaglig kompetanse. For at samfunnet skal digitaliseres er det nødvendig med ledere som har både teknisk og økonomisk kompetanse. ITBU er lagt opp med høyt fokus på praktisk erfaring innenfor teamarbeid og kommunikasjon. Studiet søker å utdanne dyktige endringsagenter som kan effektivisere arbeidsprosesser og implementere digitale løsninger i bedrifter.\n' ,
+    jobbannonser: 'Vi tilbyr promotering av stillingsannonser ut til våre 600 dyktige studenter. I tillegg til annonsepublisering på vår nettside, har dere mulighet til å angi målgruppe og ønsket kommunikasjonskanal. Enten analogt eller digitalt.',
     forumText2: 'Dersom dere ønsker noe utenfor de vanlige rammene, huk gjerne av på “Alternativt arrangement” og skriv en kommentar om hva dere kunne tenkt dere å gjøre i kommentarfeltet. Vi i Abakus ønsker å kunne tilby et bredt spekter av arrangementer som er gunstig for våre studenter.',
     forumText1: 'Kommentarfeltet kan også brukes til å spesifisere annen informasjon, som for eksempel hvilken teknologi dere ønsker å lære bort hvis dere får faglig arrangement',
 
-    bedrifter: 'En vanlig bedpres hos oss består av en presentasjon på 45 min etterfulgt av bespisning og mingling. Som oftest blir presentasjonen avholdt på campus Kalvskinnet før vi drar til en restaurant for bespisning. Vi kan også tilrettelegge for mingling og bespisning på campus dersom dette er ønskelig. ',
+    bedrifter: 'En bedriftspresentasjon gir dere som organisasjon mulighet til å presentere dere for TIHLDE sine studenter. Dette er en gylden mulighet til å gjøre studentene bevisst på hvem dere er, hva dere tilbyr og hvordan dere jobber.\n' +
+        '\n' +
+        'Etter selve presentasjonen kan vi tilrettelegge for bespisning på restaurant eller andre steder. Under bespisningen kan bedriftsrepresentantene mingle uformelt med studentene for å bli bedre kjent. \n' +
+        'Ønskes det speed-intervjuer vil vi også være behjelpelig med det praktiske rundt dette.',
 };
