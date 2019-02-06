2ba4d231dcdc2769b18a7bf2e8dd3d01b5145089
Halvor Fladsrud Bø <halvorfb@stud.ntnu.no>
2019-02-06 18:29:27 +0100

    fix

--- src/settings.js
+++ src/settings.js
@@ -2,7 +2,7 @@
 // which are reasonable to want to change.
 
 export const TIHLDE_API = {
-    URL: 'https://tihlde-api.herokuapp.com/v1/',
+    URL: '/api/v1/',
 };
 
 export const TOKEN_HEADER_NAME = 'X-CSRF-Token';
