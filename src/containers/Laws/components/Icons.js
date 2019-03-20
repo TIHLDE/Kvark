c1f50214b34e79fea89de86d2953e54dc1a36122
William Chakroun Jacobsen <williacj@stud.ntnu.no>
2019-03-20 18:08:05 +0100

    added WOLF/Tyler

---
+++ src/containers/Laws/components/Icons.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {withStyles} from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+import classNames from 'classnames';
+
+// Material UI Components
+import Typography from '@material-ui/core/Typography';
+
+// Icons
+
+
+const styles = {
+    root: {
+        height: 300,
+        maxHeight: 300,
+        maxWidth: 250,
+        padding: 16,
+        background:'red',
+        '@media only screen and (max-width: 600px)': {
+
+        },
+    },
+    wrapper:{
+        display:'flex',
+        flexDirection:'column',
+        justifyContent: 'space-evenly',
+        alignItems: 'center'
+    },
+    image:{
+        maxHeight: 160,
+        objectFit: 'contain',
+
+    }
+};
+
+
+const Icons = (props) => {
+    const {classes, data} = props;
+    const text = (data.title)? data.title : "mangler tittel";
+    return (
+        <div className={classes.root}>
+            <div className={classes.wrapper}>
+                <img src={data.image} alt={data.alt} className={classes.image}/>
+                <Typography variant={'headline'}>{text}</Typography>
+            </div>
+        </div>
+    );
+};
+
+Icons.propTypes = {
+    classes: PropTypes.object,
+
+    data: PropTypes.object,
+    onClick: PropTypes.func,
+};
+
+export default withStyles(styles)(Icons);
--- src/containers/Laws/index.js
+++ src/containers/Laws/index.js
@@ -6,8 +6,10 @@ import Paper from '@material-ui/core/Paper';
 // Project Components
 import Navigation from '../../components/navigation/Navigation';
 import Banner from '../../components/layout/Banner';
-
+import cake from '@material-ui/icons/Cake';
+import Icons from './components/Icons';
 const bannerImage = 'http://www.f-covers.com/cover/law-and-order-special-victims-unit-2-facebook-cover-timeline-banner-for-fb.jpg';
+
 
 const styles = {
     root: {
@@ -31,12 +33,11 @@ const styles = {
         },
     },
     container:{
-        width:'100%',
-        maxHeight: 400,
-        height: 400,
+        maxHeight: 800,
+        height: 800,
         display: 'grid',
-        gridTemplateColumns: '10px auto 10px',
         gridTemplateRows: '50% 50%',
+        gridGap: 100
     },
     icons:{
         display: 'flex',
@@ -57,9 +58,22 @@ class Laws extends Component {
                         <Banner title='Law and Order' image={bannerImage}/>
                         <Paper className={classes.container}>
                             <div className={classes.icons}>
-
+                                <Icons data={{
+                                    title: "Styreinstruks for hovedstyret",
+                                    image: "http://hdwpro.com/wp-content/uploads/2017/01/3D-Cool-Image.jpg",
+                                    alt: "this is an alt text"
+                                }}/>
+                                <Icons data={{
+                                           title: "Lover for TIHLDE",
+                                           image: "http://hdwpro.com/wp-content/uploads/2017/01/3D-Cool-Image.jpg",
+                                           alt: "this is an alt text"
+                                       }}/>
+                                <Icons data={{
+                                           title: "Undergruppeinstrukser",
+                                           image: "http://hdwpro.com/wp-content/uploads/2017/01/3D-Cool-Image.jpg",
+                                           alt: "this is an alt text"
+                                       }}/>
                             </div>
-
                         </Paper>
                     </div>
                 </div>
