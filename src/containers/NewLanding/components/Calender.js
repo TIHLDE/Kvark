d6c60148ae65665269d33fafe342d0f61e26dd45
Ådne Wasbø <superspeed500.personal@gmail.com>
2019-04-05 18:01:34 +0200

    WIP: Calendar list.

--- src/containers/NewLanding/components/Calender.js
+++ src/containers/NewLanding/components/Calender.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
+import classNames from 'classnames';
 
 // Material-UI
 import { withStyles } from '@material-ui/core/styles';
@@ -10,6 +11,8 @@ import Typography from '@material-ui/core/Typography';
 
 // Icons
 import CalendarToday from '@material-ui/icons/CalendarToday';
+import Schedule from '@material-ui/icons/Schedule';
+import LocationOn from '@material-ui/icons/LocationOn';
 
 // Project componets/services
 import EventService from '../../../api/services/EventService';
@@ -29,7 +32,7 @@ const styles = theme => ({
   eventListRow: {
     display: 'flex',
     backgroundColor: 'white',
-    maxHeight: 70,
+    maxHeight: 80,
     overflow: 'hidden'
   },
   eventListContainer: {
@@ -40,8 +43,8 @@ const styles = theme => ({
     backgroundColor: 'rgba(0, 0, 0, 0.12)',
   },
   eventImageContainer: {
-    height: 70,
-    width: 70,
+    height: 80,
+    width: 80,
     overflow: 'hidden',
     display: 'inline-flex'
   },
@@ -53,15 +56,26 @@ const styles = theme => ({
     padding: 5,
   },
   eventInfo: {
-    paddingRight: '20px',
+    padding: '5px',
     textAlign: 'center',
+    width: 150,
+    overflow: 'hidden'
   },
   eventContainer: {
     display: 'flex',
     flexGrow: 1,
     '@media only screen and (max-width: 700px)': {
-      display: 'block'
+      flexDirection: 'column',
+      alignItems: 'center',
     }
+  },
+  eventInfoElement: {
+    display: 'flex',
+    alignItems: 'center',
+    width: '100%'
+  },
+  eventIcon: {
+    paddingRight: 10,
   },
   hiddenOnMobile: {
     '@media only screen and (max-width: 700px)': {
@@ -84,12 +98,21 @@ function CalendarListItem(props) {
       </div>
       <div className={classes.eventContainer}>
         <div className={classes.eventTitle}>
-          <Typography align="center" variant="title">{props.eventData.title}</Typography>
+          <Typography align='center' variant='title'>{props.eventData.title}</Typography>
         </div>
         <div className={classes.eventInfo}>
-          <div><CalendarToday/> {start.format('DD.MM.YYYY')}</div>
-          <div className={classes.hiddenOnMobile}>{start.format('HH:mm')}</div>
-          <div className={classes.hiddenOnMobile}>{props.eventData.location}</div>
+          <div className={classes.eventInfoElement}>
+            <CalendarToday className={classes.eventIcon}/>
+            {start.format('DD.MM.YYYY')}
+          </div>
+          <div className={classNames(classes.hiddenOnMobile, classes.eventInfoElement)}>
+            <Schedule className={classes.eventIcon}/>
+            {start.format('HH:mm')}
+          </div>
+          <div className={classNames(classes.hiddenOnMobile, classes.eventInfoElement)}>
+            <LocationOn className={classes.eventIcon} />
+            {props.eventData.location}
+          </div>
         </div>
       </div>
     </div>
@@ -98,11 +121,17 @@ function CalendarListItem(props) {
 
 function CalendarListView(props) {
   const {classes} = props
+  const eventsToDisplay = 3
   return (
     <Paper className={classes.eventListContainer}>
-      {props.events && props.events.map((eventData) => {
-        return (<CalendarListItem classes={classes} eventData={eventData} />);
+      {props.events && props.events.map((eventData, index) => {
+        if (index < eventsToDisplay){
+          return (<CalendarListItem key={index} classes={classes} eventData={eventData} />);
+        }
       })}
+      <div className={classes.eventListRow}>
+        {props.events && eventsToDisplay <  props.events.length ? <Typography align='center'>Se {props.events.length - eventsToDisplay} flere arrangementer.</Typography> : null}
+      </div>
     </Paper>
   )
 }
@@ -152,6 +181,12 @@ class Calender extends React.Component {
 
   componentDidMount() {
     EventService.getEvents().then((eventObject) => {
+
+      // Temp code for creating enough test data.
+      for (let i = 0; i < 15; i++) {
+        eventObject.push(eventObject[0])
+      }
+
      this.setState({events: eventObject});
     });
   }
