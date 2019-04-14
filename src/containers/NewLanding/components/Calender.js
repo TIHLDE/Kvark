b645a66f367243745ab21599c6a7ec7dea7a6047
Zenjjim <zaim.imran@gmail.com>
2019-04-14 23:37:04 +0200

    calender component

--- src/containers/NewLanding/components/Calender.js
+++ src/containers/NewLanding/components/Calender.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import moment from 'moment';
+//import './style.css';
+
 
 // Material-UI
 import { withStyles } from '@material-ui/core/styles';
@@ -7,6 +11,8 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
+import Next from '@material-ui/icons/NavigateNext';
+import Before from '@material-ui/icons/NavigateBefore';
 
 // Project componets/services
 import EventService from '../../../api/services/EventService';
@@ -18,117 +24,271 @@ import DateRange from '@material-ui/icons/DateRange';
 
 // Styles
 const styles = (theme) => ({
-  root: {
+  body: {
+    fontFamily: "'Open Sans', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+    fontSize: '1em',
+    fontWeight: 300,
+    lineHeight: '1.5',
+    color: '#777',
+    background: '#f9f9f9',
+    position: 'relative',
+  },
+  '*': {
+    boxSizing: 'border-box',
+  },
+  row: {
+    margin: 0,
+    padding: 0,
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    width: '100%',
+  },
+  rowMiddle: {
+    alignItems: 'center',
+  },
+  col: {
     flexGrow: 1,
-    padding: 0, // MUI Grid requires a padding of half the given spacing.
+    flexBasis: 0,
+    maxWidth: '100%',
   },
-  paper: {
-    padding: theme.spacing.unit,
-    color: theme.palette.text.secondary,
+  colStart: {
+    justifyContent: 'flex-start',
+    textAlign: 'left',
+  },
+  colCenter: {
+    justifyContent: 'center',
     textAlign: 'center',
+    color: '#000',
   },
-  container: {
-    maxWidth: 700,
-    width: '100vw',
-    backgroundColor: 'rgba(0, 0, 0, 0.12)',
+  colEnd: {
+    justifyContent: 'flex-end',
+    textAlign: 'right',
   },
-  tabs: {
-    marginBottom: 1,
-    backgroundColor: 'white',
+  calendar: {
+    display: 'block',
+    position: 'relative',
+    width: '100%',
+    background: '#fff',
+    border: '1px solid #eee',
+  },
+  header: {
+    textTransform: 'uppercase',
+    fontWeight: 700,
+    fontSize: '150%',
+    padding: '1.5em 0',
+    borderBottom: '1px solid #eee',
+  },
+  icon: {
+    cursor: 'pointer',
+    transition: '.15s ease-out',
+    '&:hover': {
+      transform: 'scale(1.1)',
+      transition: '.25s ease-out',
+      color: '#6eb0d6',
+    },
+    '&:first-of-type': {
+      marginLeft: '1em',
+    },
+    '&:last-of-type': {
+      marginRight: '1em',
+    },
+  },
+  days: {
+    textTransform: 'uppercase',
+    fontWeight: 400,
+    color: '#ccc',
+    fontSize: '70%',
+    padding: '.75em 0',
+    borderBottom: '1px solid #f9f9f9',
+  },
+  bodyCell: {
+    position: 'relative',
+    height: '5em',
+    borderRight: '1px solid #eee',
+    overflow: 'hidden',
+    cursor: 'pointer',
+    background: '#fff',
+    transition: '0.25s ease-out',
+    '&:hover':{
+      background: '#f9f9f9',
+      transition: '0.5s ease-out',
+    },
+    '&:last-child': {
+      borderRight: 'none',
+    },
+  },
+  bodySelected:{
+    'border-left': '10px solid transparent',
+    'border-image': 'linear-gradient(45deg, #1d448c 0%,#53cbf1 40%)',
+    'border-image-slice': 1,
+  },
+  bodyRow: {
+    'border-bottom': '1px solid #eee',
+    '&:last-child': {
+      'border-bottom': 'none',
+    },
+  },
+  bodyCellNumber: {
+    position: 'absolute',
+    fontSize: '82.5%',
+    lineHeight: 1,
+    top: '.75em',
+    right: '.75em',
+    fontWeight: '700',
+  },
+  disabled: {
+    color: '#ccc',
+    pointerEvents: 'none',
+  },
+  bodyCellBG: {
+    fontWeight: 700,
+    lineHeight: 1,
+    color: '#1d448c',
+    opacity: 0,
+    fontSize: '8em',
+    position: 'absolute',
+    top: '-.2em',
+    right: '-.05em',
+    transition: '.25s ease-out',
+    letterSpacing: '-.07em',
+  },
+  bodyCol: {
+    flexGrow: 0,
+    flexBasis: 'calc(100%/7)',
+    width: 'calc(100%/7)',
+  },
+  bodyCellHoverBG: {
+    '&:hover': {
+      opacity: 0.05,
+      transition: '.5s ease-in',
+    }
+   },
+  selectedBG: {
+    opacity: 0.05,
+    transition: '.5s ease-in',
   },
 });
 
-function FormRow(props) {
-  const { classes } = props;
-
-  return (
-    <React.Fragment>
-      <Grid item xs={1}>
-        <Paper className={classes.paper}>item</Paper>
-      </Grid>
-      <Grid item xs={1}>
-        <Paper className={classes.paper}>item</Paper>
-      </Grid>
-      <Grid item xs={1}>
-        <Paper className={classes.paper}>item</Paper>
-      </Grid>
-      <Grid item xs={1}>
-        <Paper className={classes.paper}>item</Paper>
-      </Grid>
-      <Grid item xs={1}>
-        <Paper className={classes.paper}>item</Paper>
-      </Grid>
-      <Grid item xs={1}>
-        <Paper className={classes.paper}>item</Paper>
-      </Grid>
-      <Grid item xs={1}>
-        <Paper className={classes.paper}>item</Paper>
-      </Grid>
-    </React.Fragment>
-  );
-}
-
-FormRow.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
 class Calender extends React.Component {
+
+  
+  renderHeader() {
+    const { classes } = this.props;
+    return (
+      <div className={classNames(classes.header, classes.row)}>
+        <div className={classNames(classes.col, classes.colStart)}>
+          <div className={classes.icon} onClick={this.prevMonth}>
+            <Before/>
+          </div>
+        </div>
+        <div className={classNames(classes.col, classes.colCenter)}>
+          <span>
+            {moment(this.state.currentMonth).format(this.state.dateFormat)}
+            {this.state.count}
+          </span>
+        </div>
+        <div className={classNames(classes.col, classes.colEnd)} onClick={this.nextMonth}>
+          <div className={classes.icon}><Next/></div>
+        </div>
+      </div>
+    );
+  }
+
+  renderDays() {
+    const { classes } = this.props;
+
+    const dateFormat = "dddd";
+    const days = [];
+    let startDate = moment(this.state.currentMonth).startOf('month');
+    for (let i = 0; i < 7; i++) {
+      days.push(
+        <div className={classNames(classes.col, classes.colCenter)} key={i}>
+          {moment(startDate).add(i, 'days').format(dateFormat)}
+        </div>
+      );
+    }
+    return <div className={classNames(classes.days, classes.row)}>{days}</div>;
+  }
+
+  renderCells() {
+    const { classes } = this.props;
+
+    const { currentMonth, selectedDate } = this.state;
+    const monthStart = moment(currentMonth).startOf('month');
+    const monthEnd = moment(currentMonth).endOf('month');
+    const startDate = moment(monthStart).startOf('week');
+    const endDate = moment(monthEnd).startOf('week');
+    const dateFormat = "D";
+    const rows = [];
+    let days = [];
+    let day = startDate;
+    let formattedDate = "";
+    while (day <= endDate) {
+      for (let i = 0; i < 7; i++) {
+        formattedDate = moment(day).format(dateFormat);
+        const cloneDay = day;
+        days.push(
+          <div
+            className={classNames(classes.col, classes.bodyCell, classes.bodyCellHoverBG, 
+              !moment(day).isSame(moment(monthStart), 'month')
+                ? classes.disabled
+                : moment(day).isSame(moment(selectedDate), 'days') ? classNames(classes.bodySelected, classes.bodySelected) : ""
+            )}
+            key={day}
+            onClick={() => this.onDateClick(moment(cloneDay))}
+          >
+            <span className={classes.bodyCellNumber}>{formattedDate}</span>
+            <span className={classes.bodyCellBG}>{formattedDate}</span>
+          </div>
+        );
+        day = moment(day).add(1, 'days');
+      }
+      rows.push(
+        <div className={classes.row} key={day}>
+          {days}
+        </div>
+      );
+      days = [];
+    }
+    return <div className={classes.body}>{rows}</div>;
+  }
+
+  onDateClick = day => {
+    this.setState({
+      // selectedDate: day
+    });
+  };
+
+  nextMonth = () => {
+    this.setState({
+      currentMonth: moment(this.state.currentMonth).add(1, 'months').format(this.state.dateFormat),
+    });
+  };
+
+  prevMonth = () => {
+    this.setState({
+      currentMonth: moment(this.state.currentMonth).subtract(1, 'months').format(this.state.dateFormat),
+    });
+  };
 
   constructor(props) {
     super(props);
     this.state = {
-      events: null,
-      calendarViewMode: 0,
+      currentMonth: new Date(),
+      selectedDate: new Date(),
+      dateFormat: "MMMM YYYY",
     };
-    this.handleChange = this.handleChange.bind(this);
   }
-
-  componentDidMount() {
-    EventService.getEvents().then((eventObject) => {
-
-      // // Temp code for creating enough test data.
-      // for (let i = 0; i < 15; i++) {
-      //   eventObject.push(eventObject[0]);
-      // }
-
-     this.setState({events: eventObject});
-    });
-  }
-
-  handleChange(event, newValue) {
-    this.setState({calendarViewMode: newValue});
-  }
-
+  
   render() {
     const { classes } = this.props;
-    let spacing = '8';
 
     return (
-      <div className={classes.root}>
-        <Paper className={classes.container}>
-          <Tabs centered className={classes.tabs} value={this.state.calendarViewMode} onChange={this.handleChange}>
-            <Tab icon={<Reorder/>} label='Listevisning' />
-            <Tab icon={<DateRange/>} label='Kalendervisning' />
-          </Tabs>
-            {this.state.calendarViewMode === 0 ?
-            <CalendarListView events={this.state.events} />
-            :
-            <Grid container spacing={Number(spacing)}>
-              <Grid container justify="center" alignItems="center" item xs={12} spacing={Number(spacing)}>
-                <FormRow classes={classes} />
-              </Grid>
-              <Grid container justify="center" alignItems="center" item xs={12} spacing={Number(spacing)}>
-                <FormRow classes={classes} />
-              </Grid>
-              <Grid container justify="center" alignItems="center" item xs={12} spacing={Number(spacing)}>
-                <FormRow classes={classes} />
-              </Grid>
-              <Grid container justify="center" alignItems="center" item xs={12} spacing={Number(spacing)}>
-                <FormRow classes={classes} />
-              </Grid>
-            </Grid>
-            }
-        </Paper>
+      <div className={classes.calendar}>
+        {this.renderHeader()}
+        {this.renderDays()}
+        {this.renderCells()}
       </div>
     );
   }
---
+++ src/containers/NewLanding/components/Calender__css.js
@@ -0,0 +1,185 @@
+import React from 'react';
+import PropTypes, { array } from 'prop-types';
+import moment from 'moment';
+import './style.css';
+
+// Material-UI
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
+import Next from '@material-ui/icons/NavigateNext';
+import Before from '@material-ui/icons/NavigateBefore';
+
+// Project componets/services
+import CalendarListView from './CalendarListView';
+import Schedule from '@material-ui/icons/Schedule';
+
+
+// Icons
+import Reorder from '@material-ui/icons/Reorder';
+import DateRange from '@material-ui/icons/DateRange';
+
+// Styles
+const styles = (theme) => ({
+  event: {
+    width: '100%',
+    color: '#1d448c',
+    backgroundColor: 'rgba(110,176,214, 0.15)',
+    padding: '5px',
+    marginBottom: '3px',
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+    fontSize: '12px',
+    textOverflow: 'ellipsis',
+  },
+  list: {
+    width: '100%',  
+    position: 'absolute',
+    top: '10%',
+  }
+});
+
+class Calender extends React.Component {
+
+  renderHeader() {
+    return (
+      <div className="header row flex-middle">
+        <div className="col col-start">
+          <div className="icon" onClick={this.prevMonth}>
+            <Before/>
+          </div>
+        </div>
+        <div className="col col-center">
+          <span>
+            {moment(this.state.currentMonth).format(this.state.dateFormat)}
+            {this.state.count}
+          </span>
+        </div>
+        <div className="col col-end" onClick={this.nextMonth}>
+          <div className="icon"><Next/></div>
+        </div>
+      </div>
+    );
+  }
+
+  renderDays() {
+    const dateFormat = "dddd";
+    const days = [];
+    let startDate = moment(this.state.currentMonth).startOf('month');
+    for (let i = 0; i < 7; i++) {
+      days.push(
+        <div className="col col-center" key={i}>
+          {moment(startDate).add(i, 'days').format(dateFormat)}
+        </div>
+      );
+    }
+    return <div className="days row">{days}</div>;
+  }
+
+  renderCells() {
+    const { currentMonth, selectedDate } = this.state;
+    const monthStart = moment(currentMonth).startOf('month');
+    const monthEnd = moment(currentMonth).endOf('month');
+    const startDate = moment(monthStart).startOf('week');
+    const endDate = moment(monthEnd).startOf('week');
+    const dateFormat = "D";
+    const rows = [];
+    let days = [];
+    let day = startDate;
+    let formattedDate = "";
+    while (day <= endDate) {
+      for (let i = 0; i < 7; i++) {
+        formattedDate = moment(day).format(dateFormat);
+        const cloneDay = day;
+        days.push(
+          <div
+            className={`col cell ${
+              !moment(day).isSame(moment(monthStart), 'month')
+                ? "disabled"
+                : moment(day).isSame(moment(selectedDate), 'days') ? "selected" : ""
+            }`}
+            key={day}
+            onClick={() => this.onDateClick(moment(cloneDay))}
+          >
+            <span className="number">{formattedDate}</span>
+            <span className="bg">{formattedDate}</span>
+            {this.displayEvent(day)}
+          </div>
+        );
+        day = moment(day).add(1, 'days');
+      }
+      rows.push(
+        <div className="row" key={day}>
+          {days}
+        </div>
+      );
+      days = [];
+    }
+    return <div className="body">{rows}</div>;
+  }
+
+  onDateClick = day => {
+    this.setState({
+      // selectedDate: day
+    });
+  };
+
+  nextMonth = () => {
+    this.setState({
+      currentMonth: moment(this.state.currentMonth).add(1, 'months').format(this.state.dateFormat),
+    });
+  };
+
+  prevMonth = () => {
+    this.setState({
+      currentMonth: moment(this.state.currentMonth).subtract(1, 'months').format(this.state.dateFormat),
+    });
+  };
+
+  displayEvent(date) {
+    const { classes } = this.props;
+    date = moment(date).format('DD-MM-YY');
+    const eventText = [];
+    if(this.props.events !== null){
+      this.props.events.forEach(event => {
+          let eventDate = moment(event.start).format('DD-MM-YY');
+          if(eventDate === date){ 
+            eventText.push( 
+              <li className={classes.event}>{event.title}</li>,
+            )
+          }
+      });
+      if(eventText.length > 0){
+        return <ul className={classes.list}>{eventText}</ul>;
+      }
+    }
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentMonth: new Date(),
+      selectedDate: new Date(),
+      dateFormat: "YYYY MMMM",
+    };
+  }
+  
+  render() {
+    return (
+      <div className="calendar">
+        {this.renderHeader()}
+        {this.renderDays()}
+        {this.renderCells()}
+      </div>
+    );
+  }
+}
+
+Calender.propTypes = {
+  classes: PropTypes.object.isRequired,
+  events: PropTypes.array,
+};
+
+export default withStyles(styles)(Calender);
