b87b61b721ec62debf4695b36ba4fed8a4660cf4
olros <31009729+olros@users.noreply.github.com>
2019-10-09 14:29:31 +0200

    Add auth check before loading userdata (#33)

--- src/api/services/UserService.js
+++ src/api/services/UserService.js
@@ -1,21 +1,27 @@
 import API from '../api';
 import store from '../../store/store';
 import * as UserActions from '../../store/actions/UserActions';
+import AuthService from '../../api/services/AuthService';
 
 class UserService {
     
     static getUserData = async () => {
-        // Check store
-        const user = store.getState().user.userData;
-        if(user.first_name !== undefined) {
-            return Promise.resolve(user);
+        // Check if the user is logged in
+        if (AuthService.isAuthenticated()) {
+            // Check store
+            const user = store.getState().user.userData;
+            if(user.first_name !== undefined) {
+                return Promise.resolve(user);
+            } else {
+                // Fetch
+                return API.getUserData().response()
+                    .then((data) => {
+                        UserActions.setUserData(data)(store.dispatch);
+                        return UserActions.getUserData(store.getState()).userData;
+                    });
+            }
         } else {
-            // Fetch
-            return API.getUserData().response()
-                .then((data) => {
-                    UserActions.setUserData(data)(store.dispatch);
-                    return UserActions.getUserData(store.getState()).userData;
-                });
+            return null;
         }
     }
 
