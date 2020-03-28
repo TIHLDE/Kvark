5f0ba1f5fe3b6039f197def1671bd9068aa3ecad
Olaf Rosendahl <olafrosendahl@gmail.com>
2020-03-28 17:23:46 +0100

    Update group descriptions (#124)

--- src/text/AboutText.js
+++ src/text/AboutText.js
@@ -5,36 +5,26 @@ export default {
     history2: 'TIHLDE er en ikke-profitabel og politisk nøytral organisasjon som baserer seg på frivillig engasjement fra studenter. I tillegg til hovedstyret (HS), har TIHLDE pr idag 4 undergrupper: drift, næringsliv og kurs, sosialen og promo, og 6 komitéer: KosKom, TurKom, Brettspill, Redaksjonen, NetKom, og FadderKom. I tillegg har TIHLDE 2 interessegrupper: SEMIKOLON og TIHLDE PYTHONS',
 
     drift: 'Drift er en gruppe på 9 studenter som tar seg av drifting av TIHLDEs datasystemer. Her inngår bla. medlemstjeneren Colargol, studenttjeneren Balthazar, VM-parken Nerdvana samt andre støttetjenere. Oppgavene til Drift er brukeradministrasjon, backup, installasjon og oppgradering av programvare, DNS-administrering, brannslukking og annet. ',
-    social: 'Sosialen jobber for å fremme sosiale aktiviteter blant medlemmene. Det være seg sosiale tilstelninger som fester, kino, bowling, gokart mm. Fantasien er grensen, og forslag til aktiviteter mottas med smil! :)',
+    social: 'Sosialen sørger for samhold på tvers av alle linjene i TIHLDE! De arrangerer alt fra LAN til fester og hytteturer. Sosialen er med å bidra til at alle medlemmene i TIHLDE skal bli kjent, trives og ha det gøy på tvers av studiene. Sosialen er alltid åpen for engasjerte medlemmer med gode ideer.',
     business: 'Næringsliv og Kurs, tidligere Orakel, jobber for å fremme det faglige tilbudet til medlemmene. Her inngår bla. bedriftspresentasjoner, kurs, foredrag, nærlingslivsdag og lignende.',
     promo: 'Promo har som oppgave å informere studenter og medlemmer om TIHLDEs saker og arrangementer, i tillegg til å dokumentere hendinger som har vært.',
 
     jubkom: `Jubileumskomitéen til TIHLDE aktiveres hvert jubileums år, vanligvis hvert 5 år.
     JubKom sitt virke går ut på å markere linjeforeningen, og dens historie for TIHLDE medlemmer. Et jubileum er ikke bare en begivenhet for TIHLDE og dets medlemmer, men også ansatte ved AIT, De Eldstes Raad, og ikke minst alle tidligere medlemmer av TIHLDE.
     Det er JubKom sin jobb å nå ut til gamle ildsjeler, og invitere dem tilbake for at de kan se hvordan linjeforeningen ser ut i dag.!`,
-    jubkom2: 'Neste jubileums år er i 2023, da blir THLDE 30 år',
 
     devkom: `DevKom ble utviklet med formål om å utforme en ny nettside til TIHLDE.
-    I dag jobber det engasjerte og flinke studenter med nettopp dette.
-    DevKom er nylig oppstartet, men de kommer allerede til å ha første versjon av den nye nettsiden klar allerede 1. september 2018.`,
-    devkom2: `I DevKom er det ikke bare dine ferdigheter som web-utvikler som er viktige, men også hvordan du jobber med andre. Dersom du er flink til å jobbe i lag, og liker å jobbe med nettsider, så er DevKom noe for deg, og vi oppfordrer deg til å søke for å bli med.
-    <a target="_blank" rel="noopener noreferrer" href="https://forms.gle/DkA8RVAdcmfFyDPu7">Søk her</a>`,
-
-    turkom: `TurKomitéen er en helt ny komité som ble startet av engasjerte og turglade studenter, etter ønske fra Generalforsamlingen.
+    Siden den ble lansert har DevKom jobbet kontinuerlig med videre utvikling av ny funksjonalitet på nettsiden.
+    I dag jobber det engasjerte og dyktige studenter med nettopp dette. Medlemmene har stor frihet til å jobbe med det de selv synes er mest interessant.`,
+ 
+    turkom: `TurKomitéen er en komité som ble startet av engasjerte og turglade studenter, etter ønske fra Generalforsamlingen.
     TurKom skal gi turglede hos TIHLDEs medlemmer, og skal gjøre dette ved å arrangere telt- og overnattingsturer 2 – 3 ganger i semesteret.
     Takket være SiT, har TurKom kunne kjøpe utmerket turutstyr til dette formålet.
     Om du trives ute i norsk natur, og ønsker å utforske Trondheims natur sammen med likesinnede studenter, så burde du definitivt bli med på tur.
     Følg med på tihlde.org, vår <a target="_blank" rel="noopener noreferrer" href="http://facebook.com/tihlde">Facebookside</a>, og vår <a target="_blank" rel="noopener noreferrer" href="https://calendar.google.com/calendar?cid=NmJkZDF2N24wOHNtOGE3NDRpYWlvaTQ4ZG9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ">kalender</a>, så du ikke går glipp av turene!`,
 
-    turkom2: `Dersom du er en turglad student, og ønsker å gi andre studenter gode turopplevelser, så burde du søke opptak til TurKom.
-    Opptaket starter 31. august i år. Følg med på tihlde.org, vår <a target="_blank" rel="noopener noreferrer" href="http://facebook.com/tihlde"> for informasjon rundt opptaket.`,
-
-    kosekom: `Kosekomitéen er den koseligste komitéen i TIHLDE.
-    KosKom sitt oppdrag er å spre kos, glede og god stemning gjennom linjeforeningen, dets medlemmer og dets frivillige.
-    KosKom sin operasjon sentreres rundt TIHLDE-rommet, hvor de sørger for å skape en varm atmosfære med kaffe, videospill, og iblant nystekte vafler.`,
-    kosekom2: `Om du er et koselig individ som ønsker å spre smil og godt humør, så må du bli med i KosKom!
-    KosKom er direkte underlagt undergruppen Sosialen, og opptak til KosKom skjer gjennom Sosialen.
-    Om du ønsker å være med, så åpner Sosialen for opptak 31. august i år. Følg med på tihlde.org, og vår <a target="_blank" rel="noopener noreferrer" href="http://facebook.com/tihlde">Facebookside</a>, for informasjon rundt opptaket.`,
+    kontkom: `KontorKomitén har ansvaret for kontoret og TIHLDEs kioskvirksomhet.
+    I tillegg til å være kontoransvarlige, jobber også komitéen med å arrangere flere koselige arrangementer, som er med på å spre glede gjennom semesteret for TIHLDEs medlemmer!`,
 
     arrkom: `Arrangementskomitéen til TIHLDE har ansvar for alle store sosiale arrangementer utenom rene fester. Dette er naturlig nok stor variasjon i typen arrangementer, og jobben er derfor svært variert. Arrangementer som galla, LAN, og Julebord er typiske arrangementer som ArrKom planlegger og gjennomfører.`,
     arrkom2: `ArrKom er direkte underlagt undergruppen Sosialen, og opptak til ArrKom skjer gjennom Sosialen.
