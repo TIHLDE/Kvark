ea464d37404ba48b6b31a5298e76fbd63a1c4ea2
Olaf Rosendahl <olafrosendahl@gmail.com>
2020-11-09 20:46:44 +0100

    feat(loading): lazy loading (#302)
    * Update .gitignore
    
    * feat(lazy): make some pages lazy load
    
    * feat(files): remove unused and update tihlde logo
    
    * Update index.js
    
    * feat(loading): add lazy loading pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import 'assets/css/index.css';
 import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom';
@@ -6,7 +6,7 @@ import URLS from 'URLS';
 import GA from 'analytics';
 import 'delayed-scroll-restoration-polyfill';
 
-// Service and action imports
+// Services
 import { ThemeProvider } from 'context/ThemeContext';
 import { useAuth } from 'api/hooks/Auth';
 import { useMisc, MiscProvider } from 'api/hooks/Misc';
@@ -16,35 +16,38 @@ import { JobPostProvider } from 'api/hooks/JobPost';
 import { EventProvider } from 'api/hooks/Event';
 import { SnackbarProvider } from './api/hooks/Snackbar';
 
+// Project components
+import MessageGDPR from 'components/miscellaneous/MessageGDPR';
+import Navigation from 'components/navigation/Navigation';
+
 // Project containers
-import EventDetails from 'containers/EventDetails';
-import Companies from 'containers/Companies';
 import About from 'containers/About';
-import ContactInfo from 'containers/ContactInfo';
-import Admin from 'containers/Admin';
-import UserAdmin from 'containers/UserAdmin';
+import Cheatsheet from 'containers/Cheatsheet';
+import Companies from 'containers/Companies';
+import EventDetails from 'containers/EventDetails';
 import Events from 'containers/Events';
-import Services from 'containers/Services';
-import EventAdministration from 'containers/EventAdministration';
-import NewStudent from 'containers/NewStudent';
-import Profile from 'containers/Profile';
 import JobPosts from 'containers/JobPosts';
 import JobPostDetails from 'containers/JobPostDetails';
-import JobPostAdministration from 'containers/JobPostAdministration';
-import LogIn from 'containers/LogIn';
-import ForgotPassword from 'containers/ForgotPassword';
-import Laws from 'containers/Laws';
 import NewLanding from 'containers/NewLanding';
-import Http404 from 'containers/Http404';
-import EventRegistration from 'containers/EventRegistration';
-import SignUp from 'containers/SignUp';
-import PrivacyPolicy from 'containers/PrivacyPolicy';
-import Cheatsheet from 'containers/Cheatsheet';
 import NewsDetails from 'containers/NewsDetails';
-import News from 'containers/News';
-import NewsAdministration from 'containers/NewsAdministration';
-import EventRules from 'containers/EventRules';
-import MessageGDPR from 'components/miscellaneous/MessageGDPR';
+import Profile from 'containers/Profile';
+const Admin = lazy(() => import('containers/Admin'));
+const ContactInfo = lazy(() => import('containers/ContactInfo'));
+const EventAdministration = lazy(() => import('containers/EventAdministration'));
+const EventRegistration = lazy(() => import('containers/EventRegistration'));
+const EventRules = lazy(() => import('containers/EventRules'));
+const ForgotPassword = lazy(() => import('containers/ForgotPassword'));
+const Http404 = lazy(() => import('containers/Http404'));
+const JobPostAdministration = lazy(() => import('containers/JobPostAdministration'));
+const Laws = lazy(() => import('containers/Laws'));
+const LogIn = lazy(() => import('containers/LogIn'));
+const News = lazy(() => import('containers/News'));
+const NewsAdministration = lazy(() => import('containers/NewsAdministration'));
+const NewStudent = lazy(() => import('containers/NewStudent'));
+const PrivacyPolicy = lazy(() => import('containers/PrivacyPolicy'));
+const Services = lazy(() => import('containers/Services'));
+const SignUp = lazy(() => import('containers/SignUp'));
+const UserAdmin = lazy(() => import('containers/UserAdmin'));
 
 // The user needs to be authorized (logged in and member of an authorized group) to access these routes
 const requireAuth = (OriginalComponent: React.ReactElement, accessGroups: Array<string> = []): React.ReactElement => {
@@ -100,7 +103,7 @@ const requireAuth = (OriginalComponent: React.ReactElement, accessGroups: Array<
     }, [isAuthenticated, getUserData]);
 
     if (isLoading) {
-      return <div>Autentiserer...</div>;
+      return <Navigation isLoading />;
     } else if (!isAuthenticated()) {
       setLogInRedirectURL(location.pathname);
       return <Navigate to={URLS.login} />;
@@ -140,48 +143,50 @@ export const Application = () => {
   return (
     <Providers>
       <BrowserRouter>
-        {GA.init() && <GA.RouteTracker />}
-        <Routes>
-          <Route element={<NewLanding />} path='/' />
-          <Route path={URLS.events}>
-            <Route element={<EventRegistration />} path=':id/registrering/' />
-            <Route element={<EventDetails />} path=':id/*' />
-            <Route element={<Events />} path='' />
-          </Route>
-          <Route element={<About />} path={URLS.about} />
-          <Route element={<ContactInfo />} path={URLS.contactInfo} />
-          <Route element={<Services />} path={URLS.services} />
-          <Route element={<Companies />} path={URLS.company} />
-          <Route element={<NewStudent />} path={URLS.newStudent} />
-          <Route element={<Profile />} path={URLS.profile} />
-          <Route path={URLS.jobposts}>
-            <Route element={<JobPostDetails />} path=':id/*' />
-            <Route element={<JobPosts />} path='' />
-          </Route>
-          <Route element={<Laws />} path={URLS.laws} />
-          <Route element={<PrivacyPolicy />} path={URLS.privacyPolicy} />
-          <Route element={<EventRules />} path={URLS.eventRules} />
-          <Route path={URLS.news}>
-            <Route element={<NewsDetails />} path=':id/*' />
-            <Route element={<News />} path='' />
-          </Route>
+        <Suspense fallback={<Navigation isLoading />}>
+          {GA.init() && <GA.RouteTracker />}
+          <Routes>
+            <Route element={<NewLanding />} path='/' />
+            <Route path={URLS.events}>
+              <Route element={<EventRegistration />} path=':id/registrering/' />
+              <Route element={<EventDetails />} path=':id/*' />
+              <Route element={<Events />} path='' />
+            </Route>
+            <Route element={<About />} path={URLS.about} />
+            <Route element={<ContactInfo />} path={URLS.contactInfo} />
+            <Route element={<Services />} path={URLS.services} />
+            <Route element={<Companies />} path={URLS.company} />
+            <Route element={<NewStudent />} path={URLS.newStudent} />
+            <Route element={<Profile />} path={URLS.profile} />
+            <Route path={URLS.jobposts}>
+              <Route element={<JobPostDetails />} path=':id/*' />
+              <Route element={<JobPosts />} path='' />
+            </Route>
+            <Route element={<Laws />} path={URLS.laws} />
+            <Route element={<PrivacyPolicy />} path={URLS.privacyPolicy} />
+            <Route element={<EventRules />} path={URLS.eventRules} />
+            <Route path={URLS.news}>
+              <Route element={<NewsDetails />} path=':id/*' />
+              <Route element={<News />} path='' />
+            </Route>
 
-          <Route element={requireAuth(<Cheatsheet />)} path={`${URLS.cheatsheet}:studyId/:classId/`} />
-          <Route element={requireAuth(<Cheatsheet />)} path={`${URLS.cheatsheet}*`} />
+            <Route element={requireAuth(<Cheatsheet />)} path={`${URLS.cheatsheet}:studyId/:classId/`} />
+            <Route element={requireAuth(<Cheatsheet />)} path={`${URLS.cheatsheet}*`} />
 
-          <Route element={requireAuth(<Admin />, ['HS', 'Promo', 'Nok', 'Index'])} path={URLS.admin} />
-          <Route element={requireAuth(<UserAdmin />, ['HS', 'Index'])} path={URLS.userAdmin} />
-          <Route element={requireAuth(<JobPostAdministration />, ['HS', 'Nok', 'Index'])} path={URLS.jobpostsAdmin} />
-          <Route element={requireAuth(<EventAdministration />, ['HS', 'Promo', 'Nok', 'Index'])} path={URLS.eventAdmin} />
-          <Route element={requireAuth(<NewsAdministration />, ['HS', 'Promo', 'Nok', 'Index'])} path={URLS.newsAdmin} />
+            <Route element={requireAuth(<Admin />, ['HS', 'Promo', 'Nok', 'Index'])} path={URLS.admin} />
+            <Route element={requireAuth(<UserAdmin />, ['HS', 'Index'])} path={URLS.userAdmin} />
+            <Route element={requireAuth(<JobPostAdministration />, ['HS', 'Nok', 'Index'])} path={URLS.jobpostsAdmin} />
+            <Route element={requireAuth(<EventAdministration />, ['HS', 'Promo', 'Nok', 'Index'])} path={URLS.eventAdmin} />
+            <Route element={requireAuth(<NewsAdministration />, ['HS', 'Promo', 'Nok', 'Index'])} path={URLS.newsAdmin} />
 
-          <Route element={<LogIn />} path={URLS.login} />
-          <Route element={<ForgotPassword />} path={URLS.forgotPassword} />
-          <Route element={<SignUp />} path={URLS.signup} />
+            <Route element={<LogIn />} path={URLS.login} />
+            <Route element={<ForgotPassword />} path={URLS.forgotPassword} />
+            <Route element={<SignUp />} path={URLS.signup} />
 
-          <Route element={<Http404 />} path='*' />
-        </Routes>
-        <MessageGDPR />
+            <Route element={<Http404 />} path='*' />
+          </Routes>
+          <MessageGDPR />
+        </Suspense>
       </BrowserRouter>
     </Providers>
   );
@@ -195,5 +200,7 @@ console.log(
 );
 // eslint-disable-next-line no-console
 console.log('%cSnoker rundt du? Det liker vi. Vi i Index ser alltid etter nye medlemmer.', 'font-weight: bold; font-size: 1rem;color: yellow;', '');
+// eslint-disable-next-line no-console
+console.log('B64: ImZsYWd7NjgzNTRjMTAtODBiZS00ZjMzLTkwMjItM2RjMjE5YjNmYTQ0fSI=');
 
 ReactDOM.render(<Application />, document.getElementById('root'));
