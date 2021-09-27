18912bd2d4878f85d64dea7b204bc2ef11cbdb64
Martin Clementz <martin.hosle@gmail.com>
2021-09-27 16:35:10 +0200

    feat(changelog):  adds captions on "om index" page

--- src/pages/Pages/specials/Index/index.tsx
+++ src/pages/Pages/specials/Index/index.tsx
@@ -3,9 +3,19 @@ import Changelog from 'pages/Pages/specials/Index/Changelog';
 import MembersCard from 'pages/Pages/specials/Index/MembersCard';
 import { Groups } from 'types/Enums';
 
+import Container from 'components/layout/Container';
+
 const AboutIndex = () => {
   return (
     <>
+      <Container>
+        <ul style={{listStyle: 'none'}}>
+          <li>✨ - Ny funksjonalitet</li>
+          <li>⚡ - Forbedret funksjonalitet</li>
+          <li>🦟 - Fikset en bug</li>
+          <li>🎨 - Designendringer</li>
+        </ul>
+      </Container>
       <Changelog changelogURL='https://raw.githubusercontent.com/tihlde/Kvark/master/CHANGELOG.md' title='Hva har vi gjort i frontend?' />
       <Changelog changelogURL='https://raw.githubusercontent.com/tihlde/Lepton/master/CHANGELOG.md' title='Hva har vi gjort i backend?' />
       <ErrorCard />
