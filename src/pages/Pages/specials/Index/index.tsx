e2927812a102710a7186b4f1bb6b1a28fed40588
Martin Clementz <martin.hosle@gmail.com>
2021-09-28 17:13:44 +0200

    feat(changelog): add captions

--- src/pages/Pages/specials/Index/index.tsx
+++ src/pages/Pages/specials/Index/index.tsx
@@ -2,20 +2,24 @@ import ErrorCard from 'pages/Pages/specials/Index/ErrorCard';
 import Changelog from 'pages/Pages/specials/Index/Changelog';
 import MembersCard from 'pages/Pages/specials/Index/MembersCard';
 import { Groups } from 'types/Enums';
+import InfoCard from 'components/layout/InfoCard';
+import { styled } from '@mui/material';
 
-import Container from 'components/layout/Container';
+const List = styled('ul')({
+  listStyleType: 'none',
+});
 
 const AboutIndex = () => {
   return (
     <>
-      <Container>
-        <ul style={{listStyle: 'none'}}>
-          <li>✨ - Ny funksjonalitet</li>
-          <li>⚡ - Forbedret funksjonalitet</li>
-          <li>🦟 - Fikset en bug</li>
-          <li>🎨 - Designendringer</li>
-        </ul>
-      </Container>
+      <InfoCard header='Tegnforklaring'>
+        <List>
+          <li>✨ Ny funksjonalitet</li>
+          <li>⚡ Forbedret funksjonalitet</li>
+          <li>🦟 Fikset en bug</li>
+          <li>🎨 Designendringer</li>
+        </List>
+      </InfoCard>
       <Changelog changelogURL='https://raw.githubusercontent.com/tihlde/Kvark/master/CHANGELOG.md' title='Hva har vi gjort i frontend?' />
       <Changelog changelogURL='https://raw.githubusercontent.com/tihlde/Lepton/master/CHANGELOG.md' title='Hva har vi gjort i backend?' />
       <ErrorCard />
