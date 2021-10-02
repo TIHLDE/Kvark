d60bb71fbbb166fe3ce56b70b1fbafaa0d1c130e
olros <olafrosendahl@gmail.com>
2021-10-02 13:48:23 +0200

    refactor(participant): simplify styling and use verifydialog

--- src/pages/EventAdministration/components/Participant.tsx
+++ src/pages/EventAdministration/components/Participant.tsx
@@ -6,8 +6,7 @@ import parseISO from 'date-fns/parseISO';
 import { useSnackbar } from 'hooks/Snackbar';
 
 // Material-ui
-import { makeStyles } from '@mui/styles';
-import { Checkbox, Typography, Collapse, Button, ListItem, ListItemText, ListItemSecondaryAction, Divider } from '@mui/material';
+import { Stack, Checkbox, Typography, Collapse, Button, ListItem, ListItemButton, ListItemText, ListItemAvatar, Divider } from '@mui/material';
 
 // Icons
 import ExpandMoreIcon from '@mui/icons-material/ExpandMoreRounded';
@@ -20,37 +19,8 @@ import ArrowUpwardIcon from '@mui/icons-material/ArrowUpwardRounded';
 
 // Project components
 import Avatar from 'components/miscellaneous/Avatar';
-import Dialog from 'components/layout/Dialog';
 import Paper from 'components/layout/Paper';
 import VerifyDialog from 'components/layout/VerifyDialog';
-
-const useStyles = makeStyles((theme) => ({
-  avatar: {
-    marginRight: theme.spacing(2),
-  },
-  paper: {
-    marginBottom: theme.spacing(1),
-    overflow: 'hidden',
-    background: theme.palette.background.smoke,
-  },
-  wrapper: {
-    paddingRight: theme.spacing(8),
-    alignItems: 'center',
-  },
-  content: {
-    display: 'grid',
-    gridGap: theme.spacing(1),
-    padding: theme.spacing(2),
-  },
-  actions: {
-    display: 'grid',
-    gridTemplateColumns: '1fr 1fr',
-    gridGap: theme.spacing(1),
-    [theme.breakpoints.down('lg')]: {
-      gridTemplateColumns: '1fr',
-    },
-  },
-}));
 
 export type ParticipantProps = {
   eventId: number;
@@ -58,12 +28,10 @@ export type ParticipantProps = {
 };
 
 const Participant = ({ registration, eventId }: ParticipantProps) => {
-  const classes = useStyles();
   const updateRegistration = useUpdateEventRegistration(eventId);
   const deleteRegistration = useDeleteEventRegistration(eventId);
   const showSnackbar = useSnackbar();
   const [checkedState, setCheckedState] = useState(registration.has_attended);
-  const [showModal, setShowModal] = useState(false);
   const [expanded, setExpanded] = useState(false);
   const { data: event } = useEventById(eventId);
 
@@ -77,7 +45,6 @@ const Participant = ({ registration, eventId }: ParticipantProps) => {
         showSnackbar(`Deltageren ble fjernet`, 'success');
       },
     });
-    setShowModal(false);
   };
 
   const handleAttendedCheck = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -86,7 +53,10 @@ const Participant = ({ registration, eventId }: ParticipantProps) => {
       { registration: { has_attended: event.target.checked }, userId: registration.user_info.user_id },
       {
         onSuccess: () => {
-          showSnackbar(`Deltageren ble satt til ${!event.target.checked ? 'ikke ' : ''}ankommet`, 'success');
+          showSnackbar(
+            `${registration.user_info.first_name} ${registration.user_info.last_name} ble satt til ${!event.target.checked ? 'ikke ' : ''}ankommet`,
+            'success',
+          );
         },
         onError: () => {
           setCheckedState(!event.target.checked);
@@ -95,39 +65,32 @@ const Participant = ({ registration, eventId }: ParticipantProps) => {
     );
   };
 
-  const changeList = (onWait: boolean) => {
-    updateRegistration.mutate({ registration: { is_on_wait: onWait }, userId: registration.user_info.user_id });
-  };
+  const changeList = (onWait: boolean) => updateRegistration.mutate({ registration: { is_on_wait: onWait }, userId: registration.user_info.user_id });
 
   return (
-    <Paper className={classes.paper} noPadding>
-      <Dialog
-        confirmText='Ja, jeg er sikker'
-        contentText={`Er du sikker på at du vil fjerne ${registration.user_info.first_name} ${registration.user_info.last_name} fra arrangementet?`}
-        onClose={() => setShowModal(false)}
-        onConfirm={deleteHandler}
-        open={showModal}
-        titleText='Er du sikker?'
-      />
-      <ListItem button className={classes.wrapper} onClick={() => setExpanded((prev) => !prev)}>
-        <Avatar className={classes.avatar} user={registration.user_info} />
-        <ListItemText
-          primary={`${registration.user_info.first_name} ${registration.user_info.last_name}`}
-          secondary={`${getUserClass(registration.user_info.user_class)} - ${getUserStudyShort(registration.user_info.user_study)}${
-            registration.user_info.allergy !== '' ? `\nAllergier: ${registration.user_info.allergy}` : ''
-          }${!registration.allow_photo ? `\nVil ikke bli tatt bilde av` : ''}`}
-        />
-        {expanded ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-        <ListItemSecondaryAction>{!registration.is_on_wait && <Checkbox checked={checkedState} onChange={handleAttendedCheck} />}</ListItemSecondaryAction>
+    <Paper bgColor='smoke' noOverflow noPadding sx={{ mb: 1 }}>
+      <ListItem disablePadding secondaryAction={!registration.is_on_wait && <Checkbox checked={checkedState} onChange={handleAttendedCheck} />}>
+        <ListItemButton onClick={() => setExpanded((prev) => !prev)}>
+          <ListItemAvatar>
+            <Avatar user={registration.user_info} />
+          </ListItemAvatar>
+          <ListItemText
+            primary={`${registration.user_info.first_name} ${registration.user_info.last_name}`}
+            secondary={`${getUserClass(registration.user_info.user_class)} - ${getUserStudyShort(registration.user_info.user_study)}${
+              registration.user_info.allergy !== '' ? `\nAllergier: ${registration.user_info.allergy}` : ''
+            }${!registration.allow_photo ? `\nVil ikke bli tatt bilde av` : ''}`}
+          />
+          {expanded ? <ExpandLessIcon sx={{ mr: 2 }} /> : <ExpandMoreIcon sx={{ mr: 2 }} />}
+        </ListItemButton>
       </ListItem>
       <Collapse in={expanded}>
         <Divider />
-        <div className={classes.content}>
+        <Stack gap={1} sx={{ p: 2 }}>
           <div>
             <Typography variant='subtitle1'>{`Epost: ${registration.user_info.email}`}</Typography>
             <Typography variant='subtitle1'>{`Påmeldt: ${formatDate(parseISO(registration.created_at))}`}</Typography>
           </div>
-          <div className={classes.actions}>
+          <Stack direction={{ xs: 'column', md: 'row' }} gap={1}>
             {registration.is_on_wait ? (
               <VerifyDialog
                 contentText={`Er du sikker på at du vil gi denne personen plass på dette arrangementet? ${
@@ -146,11 +109,15 @@ const Participant = ({ registration, eventId }: ParticipantProps) => {
                 Flytt til venteliste
               </Button>
             )}
-            <Button color='error' fullWidth onClick={() => setShowModal(true)} startIcon={<Delete />} variant='outlined'>
+            <VerifyDialog
+              color='error'
+              contentText={`Er du sikker på at du vil fjerne ${registration.user_info.first_name} ${registration.user_info.last_name} fra arrangementet?`}
+              onConfirm={deleteHandler}
+              startIcon={<Delete />}>
               Fjern deltager
-            </Button>
-          </div>
-        </div>
+            </VerifyDialog>
+          </Stack>
+        </Stack>
       </Collapse>
     </Paper>
   );
