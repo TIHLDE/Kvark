09f5866999a60d95a467b6c01ba2edc40771bf4b
Fredrik Ruud <90713524+LordFlipFlap@users.noreply.github.com>
2021-10-12 21:11:09 +0200

    Update src/components/miscellaneous/StrikeListItem.tsx
    Co-authored-by: Olaf Rosendahl <olafrosendahl@gmail.com>

--- src/components/miscellaneous/StrikeListItem.tsx
+++ src/components/miscellaneous/StrikeListItem.tsx
@@ -42,11 +42,8 @@ const StrikeListItem = ({ strike, user, isAdmin = false, allStrikes = false, ...
         <Stack gap={1} sx={{ p: 2 }}>
           <div>
             {allStrikes && (
-              <Fragment>
-                <Typography variant='subtitle2'>{`Begrunnelse: ${strike.description}`}</Typography>
-              </Fragment>
+              <Typography variant='subtitle2'>{`Begrunnelse: ${strike.description}`}</Typography>
             )}
-
             {isAdmin && Boolean(strike.creator) && (
               <Typography variant='subtitle2'>{`Opprettet av: ${strike.creator?.first_name} ${strike.creator?.last_name}`}</Typography>
             )}
