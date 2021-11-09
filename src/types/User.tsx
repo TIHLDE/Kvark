0094141bc3c5e101846c9d651aef6b52f97e9a20
Magnus Steensland <mag.steensland@gmail.com>
2021-11-09 19:43:40 +0100

    Small fixes

--- src/types/User.tsx
+++ src/types/User.tsx
@@ -1,11 +1,11 @@
 import { PermissionApp, UserClass, UserStudy } from 'types/Enums';
 import { Permissions } from 'types/Misc';
 
-export interface LoginRequestResponse {
+export type LoginRequestResponse = {
   token: string;
-}
+};
 
-export interface User {
+export type User = {
   app_token: string;
   allergy: string;
   cell: number;
@@ -23,13 +23,10 @@ export interface User {
   user_study: UserStudy;
   unanswered_evaluations_count: number;
   number_of_strikes: number;
-  start_date: string;
-  end_date: string;
-}
+};
 
 export type UserBase = Pick<User, 'user_id' | 'first_name' | 'last_name' | 'image'>;
-export type UserList = UserBase &
-  Pick<User, 'email' | 'user_class' | 'user_study' | 'allergy' | 'cell' | 'gender' | 'tool' | 'number_of_strikes' | 'start_date' | 'end_date'>;
+export type UserList = UserBase & Pick<User, 'email' | 'user_class' | 'user_study' | 'allergy' | 'cell' | 'gender' | 'tool' | 'number_of_strikes'>;
 export type UserCreate = Pick<User, 'email' | 'first_name' | 'last_name' | 'user_class' | 'user_id' | 'user_study'> & {
   password: string;
 };
