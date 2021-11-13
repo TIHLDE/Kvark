eee9404da5c88bc6d1c2acca7a824f4c4c1b9eb1
callumgran <callum.gran@gmail.com>
2021-11-13 09:38:57 +0100

    small additions

--- src/pages/Gallery/index.tsx
+++ src/pages/Gallery/index.tsx
@@ -1,5 +1,26 @@
 import React, { useState } from 'react';
+import { Theme, useMediaQuery } from '@mui/material';
+import { useGoogleAnalytics } from 'hooks/Utils';
+import { makeStyles } from '@mui/styles';
+import { useAlbums } from 'hooks/Gallery';
+import { Gallery } from 'types';
 
-const PhotoAlbum = () => {};
+const useStyles = makeStyles((theme) => ({
+  image: {
+    borderRadius: theme.shape.borderRadius,
+    border: `1px solid ${theme.palette.divider}`,
+  },
+}));
+
+export type GalleryProps = {
+  data: Gallery;
+};
+
+const GalleryRenderer = ({ data }: GalleryProps) => {
+  const { event } = useGoogleAnalytics();
+  const { data } = useAlbums();
+  const classes = useStyles();
+  const lgDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('lg'));
+};
 
 export default PhotoAlbum;
