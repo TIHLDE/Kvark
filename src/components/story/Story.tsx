9a5add98ae1a828d40ced4cb33579bf2f3639b72
Martin Clementz <martcl@stud.ntnu.no>
2022-01-27 17:13:45 +0100

    feat(stories): shows continuously hiring in stories

--- src/components/story/Story.tsx
+++ src/components/story/Story.tsx
@@ -155,7 +155,7 @@ const Story = ({ items, fadeColor }: StoryProps) => {
         newItems.push({
           ...newItem,
           link: `${URLS.jobposts}${item.id}/${urlEncode(item.title)}/`,
-          description: `Bedrift: ${item.company} \n Når: ${formatDate(parseISO(item.deadline))}`,
+          description: `Bedrift: ${item.company} \n ${item.is_continuously_hiring ? 'Fortløpende opptak' : `Når: ${formatDate(parseISO(item.deadline))}`}`,
           topText: 'Annonse',
         });
       } else if (instanceOfNews(item)) {
