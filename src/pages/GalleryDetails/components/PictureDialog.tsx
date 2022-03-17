ba06f7882541b702f9e012bfe925204abb34b8b7
Martin Clementz <martcl@stud.ntnu.no>
2022-03-17 17:37:45 +0100

    fix(gallery): fix some styling

--- src/pages/GalleryDetails/components/PictureDialog.tsx
+++ src/pages/GalleryDetails/components/PictureDialog.tsx
@@ -1,4 +1,4 @@
-import { Box, Dialog, DialogProps, Stack, Theme, Typography, useMediaQuery } from '@mui/material';
+import { Box, Dialog, DialogProps, Stack, Typography } from '@mui/material';
 
 import { Picture } from 'types';
 import { PermissionApp } from 'types/Enums';
@@ -17,46 +17,39 @@ export type PictureDialogProps = Omit<DialogProps, 'open'> & {
 
 const PictureDialog = ({ gallerySlug, picture, onClose, ...props }: PictureDialogProps) => {
   const { allowAccess } = useHavePermission([PermissionApp.PICTURE]);
-  const smDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
-  const mdDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
   return (
     <Dialog aria-labelledby={picture.title} maxWidth='lg' {...props} onClose={onClose} open>
-      <Box id='picture' sx={{ overflow: 'hidden', display: 'grid', alignContent: 'center', alignItems: 'center' }}>
-        <Stack>
-          <Box
-            component='img'
-            src={picture.image}
+      <Box id='picture' sx={{ overflow: 'scroll', maxHeight: '100%' }}>
+        <Box
+          component='img'
+          src={picture.image}
+          sx={{
+            display: 'block',
+            objectFit: 'fit',
+            maxWidth: '100%',
+            maxHeight: '80vh',
+            margin: 'auto',
+          }}
+        />
+        {(allowAccess || picture.title || picture.description) && (
+          <Stack
+            component={Paper}
+            direction='row'
+            gap={1}
             sx={{
-              display: 'block',
-              maxHeight: `${mdDown ? 'auto' : '80vh'}`,
-              height: '100%',
-              maxWidth: '100%',
-              width: `${smDown ? '85vw' : mdDown ? '60vw' : 'auto'}`,
-              margin: 'auto',
-            }}
-          />
-          {(allowAccess || picture.title || picture.description) && (
-            <Stack
-              component={Paper}
-              direction='row'
-              gap={1}
-              sx={{
-                borderTopLeftRadius: 0,
-                borderTopRightRadius: 0,
-                py: 1,
-                px: 2,
-                pb: `${smDown ? '0' : '1'}`,
-                alignItems: 'center',
-                justifyContent: 'space-between',
-              }}>
-              <div>
-                {Boolean(picture.title) && <Typography variant='h3'>{picture.title}</Typography>}
-                {Boolean(picture.description) && <Typography variant='body1'>{picture.description}</Typography>}
-              </div>
-              {allowAccess && <PictureEditorDialog gallerySlug={gallerySlug} onClose={onClose} pictureId={picture.id} />}
-            </Stack>
-          )}
-        </Stack>
+              borderTopLeftRadius: 0,
+              borderTopRightRadius: 0,
+              p: 2,
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}>
+            <div>
+              {Boolean(picture.title) && <Typography variant='h3'>{picture.title}</Typography>}
+              {Boolean(picture.description) && <Typography variant='body1'>{picture.description}</Typography>}
+            </div>
+            {allowAccess && <PictureEditorDialog gallerySlug={gallerySlug} onClose={onClose} pictureId={picture.id} />}
+          </Stack>
+        )}
       </Box>
     </Dialog>
   );
