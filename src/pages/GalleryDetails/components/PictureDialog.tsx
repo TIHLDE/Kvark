dbd03c27f49e1d53c53a115b77766eef398a1c1c
Martin Clementz <martcl@stud.ntnu.no>
2022-03-17 17:48:09 +0100

    fix(gallery): less containers

--- src/pages/GalleryDetails/components/PictureDialog.tsx
+++ src/pages/GalleryDetails/components/PictureDialog.tsx
@@ -18,39 +18,38 @@ export type PictureDialogProps = Omit<DialogProps, 'open'> & {
 const PictureDialog = ({ gallerySlug, picture, onClose, ...props }: PictureDialogProps) => {
   const { allowAccess } = useHavePermission([PermissionApp.PICTURE]);
   return (
-    <Dialog aria-labelledby={picture.title} maxWidth='lg' {...props} onClose={onClose} open>
-      <Box id='picture' sx={{ overflow: 'scroll', maxHeight: '100%' }}>
-        <Box
-          component='img'
-          src={picture.image}
+    <Dialog aria-labelledby={picture.title} maxWidth='lg' sx={{ overflow: 'scroll', maxHeight: '100%' }} {...props} onClose={onClose} open>
+      <Box
+        alt={picture.description}
+        component='img'
+        id='picture'
+        src={picture.image}
+        sx={{
+          display: 'block',
+          maxWidth: '100%',
+          maxHeight: '80vh',
+          margin: 'auto',
+        }}
+      />
+      {(allowAccess || picture.title || picture.description) && (
+        <Stack
+          component={Paper}
+          direction='row'
+          gap={1}
           sx={{
-            display: 'block',
-            objectFit: 'fit',
-            maxWidth: '100%',
-            maxHeight: '80vh',
-            margin: 'auto',
-          }}
-        />
-        {(allowAccess || picture.title || picture.description) && (
-          <Stack
-            component={Paper}
-            direction='row'
-            gap={1}
-            sx={{
-              borderTopLeftRadius: 0,
-              borderTopRightRadius: 0,
-              p: 2,
-              alignItems: 'center',
-              justifyContent: 'space-between',
-            }}>
-            <div>
-              {Boolean(picture.title) && <Typography variant='h3'>{picture.title}</Typography>}
-              {Boolean(picture.description) && <Typography variant='body1'>{picture.description}</Typography>}
-            </div>
-            {allowAccess && <PictureEditorDialog gallerySlug={gallerySlug} onClose={onClose} pictureId={picture.id} />}
-          </Stack>
-        )}
-      </Box>
+            borderTopLeftRadius: 0,
+            borderTopRightRadius: 0,
+            p: 2,
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}>
+          <div>
+            {Boolean(picture.title) && <Typography variant='h3'>{picture.title}</Typography>}
+            {Boolean(picture.description) && <Typography variant='body1'>{picture.description}</Typography>}
+          </div>
+          {allowAccess && <PictureEditorDialog gallerySlug={gallerySlug} onClose={onClose} pictureId={picture.id} />}
+        </Stack>
+      )}
     </Dialog>
   );
 };
