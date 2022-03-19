016a0e73ae4ead6650199db8cedb7fb7c3530fbc
Olaf Rosendahl <olafrosendahl@gmail.com>
2022-03-19 16:55:27 +0100

    Update PictureDialog.tsx

--- src/pages/GalleryDetails/components/PictureDialog.tsx
+++ src/pages/GalleryDetails/components/PictureDialog.tsx
@@ -18,34 +18,25 @@ export type PictureDialogProps = Omit<DialogProps, 'open'> & {
 const PictureDialog = ({ gallerySlug, picture, onClose, ...props }: PictureDialogProps) => {
   const { allowAccess } = useHavePermission([PermissionApp.PICTURE]);
   return (
-    <Dialog aria-labelledby={picture.title} maxWidth='lg' sx={{ overflow: 'scroll', maxHeight: '100%' }} {...props} onClose={onClose} open>
-      <Box
-        alt={picture.description}
-        component='img'
-        id='picture'
-        src={picture.image}
-        sx={{
-          display: 'block',
-          maxWidth: '100%',
-          maxHeight: '80vh',
-          margin: 'auto',
-        }}
-      />
+    <Dialog aria-describedby='picture-description' aria-labelledby='picture-title' maxWidth='lg' {...props} onClose={onClose} open>
+      <Box alt={picture.description} component='img' src={picture.image} sx={{ display: 'block', maxWidth: '100%', maxHeight: '80vh', margin: 'auto' }} />
       {(allowAccess || picture.title || picture.description) && (
         <Stack
           component={Paper}
           direction='row'
           gap={1}
-          sx={{
-            borderTopLeftRadius: 0,
-            borderTopRightRadius: 0,
-            p: 2,
-            alignItems: 'center',
-            justifyContent: 'space-between',
-          }}>
+          sx={{ borderTopLeftRadius: 0, borderTopRightRadius: 0, p: 2, alignItems: 'center', justifyContent: 'space-between' }}>
           <div>
-            {Boolean(picture.title) && <Typography variant='h3'>{picture.title}</Typography>}
-            {Boolean(picture.description) && <Typography variant='body1'>{picture.description}</Typography>}
+            {Boolean(picture.title) && (
+              <Typography id='picture-title' variant='h3'>
+                {picture.title}
+              </Typography>
+            )}
+            {Boolean(picture.description) && (
+              <Typography id='picture-description' variant='body1'>
+                {picture.description}
+              </Typography>
+            )}
           </div>
           {allowAccess && <PictureEditorDialog gallerySlug={gallerySlug} onClose={onClose} pictureId={picture.id} />}
         </Stack>
