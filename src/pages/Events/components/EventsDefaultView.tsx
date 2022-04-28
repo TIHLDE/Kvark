3f518500daa01652fd1d9c7db8c344161bfebf3d
Minh Dan Nguyen <minhdn@stud.ntnu.no>
2022-04-28 16:40:37 +0200

    updated eventsdefaultview

--- src/pages/Events/components/EventsDefaultView.tsx
+++ src/pages/Events/components/EventsDefaultView.tsx
@@ -7,12 +7,14 @@ import { argsToParams } from 'utils';
 
 import { useCategories } from 'hooks/Categories';
 import { useEvents } from 'hooks/Event';
+import { useIsAuthenticated } from 'hooks/User';
 import { useAnalytics } from 'hooks/Utils';
 
 import Bool from 'components/inputs/Bool';
 import Select from 'components/inputs/Select';
 import SubmitButton from 'components/inputs/SubmitButton';
 import TextField from 'components/inputs/TextField';
+import Banner from 'components/layout/Banner';
 import Expand from 'components/layout/Expand';
 import Pagination from 'components/layout/Pagination';
 import Paper from 'components/layout/Paper';
@@ -57,19 +59,22 @@ const useStyles = makeStyles()((theme) => ({
 type Filters = {
   search?: string;
   category?: string;
-  open_for_sign_up: boolean | undefined;
+  open_for_sign_up?: boolean;
+  user_favorite?: boolean;
   expired: boolean;
 };
 
 const EventsDefaultView = () => {
+  const isAuthenticated = useIsAuthenticated();
   const { event } = useAnalytics();
   const getInitialFilters = useCallback((): Filters => {
     const params = new URLSearchParams(location.search);
     const expired = params.get('expired') ? Boolean(params.get('expired') === 'true') : false;
     const open_for_sign_up = params.get('open_for_sign_up') ? Boolean(params.get('open_for_sign_up') === 'true') : undefined;
+    const user_favorite = params.get('user_favorite') ? Boolean(params.get('user_favorite') === 'true') : undefined;
     const category = params.get('category') || undefined;
     const search = params.get('search') || undefined;
-    return { expired, category, search, open_for_sign_up };
+    return { expired, category, search, open_for_sign_up, user_favorite };
   }, []);
   const { classes } = useStyles();
   const navigate = useNavigate();
@@ -85,7 +90,7 @@ const EventsDefaultView = () => {
     setValue('category', '');
     setValue('search', '');
     setValue('expired', false);
-    setFilters({ expired: false, open_for_sign_up: false });
+    setFilters({ expired: false, open_for_sign_up: false, user_favorite: false });
     navigate(`${location.pathname}${argsToParams({ expired: false })}`, { replace: true });
   };
 
@@ -112,6 +117,7 @@ const EventsDefaultView = () => {
       )}
       <Bool control={control} formState={formState} label='Tidligere' name='expired' type='switch' />
       <Bool control={control} formState={formState} label='Kun med åpen påmelding' name='open_for_sign_up' type='switch' />
+      {isAuthenticated && <Bool control={control} formState={formState} label='Favoritter' name='user_favorite' type='switch' />}
       <SubmitButton disabled={isFetching} formState={formState}>
         Søk
       </SubmitButton>
@@ -123,7 +129,7 @@ const EventsDefaultView = () => {
   );
 
   return (
-    <Page>
+    <Page banner={<Banner title='Arrangementer' />} options={{ title: 'Arrangementer' }}>
       <div className={classes.grid}>
         <div className={classes.list}>
           {isLoading && <EventListItemLoading />}
