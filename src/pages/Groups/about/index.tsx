425ed5a20b93b5ce002d9d916ce94dd59790ab3f
Minh Dan Nguyen <70275130+MinhdanNg@users.noreply.github.com>
2022-09-25 23:14:34 +0200

    Group stats (#654)
    * feat(groups): added statistics over members
    
    * merged with dev
    
    * updated groupstats so it works
    
    * removed unused and uneccessary code

--- src/pages/Groups/about/index.tsx
+++ src/pages/Groups/about/index.tsx
@@ -1,15 +1,20 @@
 import { Divider } from '@mui/material';
 import { useParams } from 'react-router-dom';
 
+import { PermissionApp } from 'types/Enums';
+
 import { useGroup } from 'hooks/Group';
-import { useIsAuthenticated } from 'hooks/User';
+import { useHavePermission, useIsAuthenticated } from 'hooks/User';
 
 import MembersCard from 'pages/Groups/about/MembersCard';
 import MembersHistoryCard from 'pages/Groups/about/MembersHistoryCard';
 
 import MarkdownRenderer from 'components/miscellaneous/MarkdownRenderer';
 
+import GroupStatistics from '../components/GroupStatistics';
+
 const GroupInfo = () => {
+  const { allowAccess: isAdmin } = useHavePermission([PermissionApp.GROUP]);
   const isAuthenticated = useIsAuthenticated();
   const { slug } = useParams<'slug'>();
   const { data: group, isLoading } = useGroup(slug || '-');
@@ -18,6 +23,7 @@ const GroupInfo = () => {
   }
   return (
     <>
+      {isAdmin && <GroupStatistics slug={group.slug} />}
       {(group.description || group.contact_email) && (
         <>
           <MarkdownRenderer value={`${group.description}${group.contact_email ? ` \n\n Kontakt: ${group.contact_email}` : ''}`} />
