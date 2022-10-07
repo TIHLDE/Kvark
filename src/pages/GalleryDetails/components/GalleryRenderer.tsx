d6d3d850a56c9e297f66f03fd4607be7969fe8cf
Callum Gran <callum.gran@gmail.com>
2022-10-07 03:09:02 +0200

    easy fix :=

--- src/pages/GalleryDetails/components/GalleryRenderer.tsx
+++ src/pages/GalleryDetails/components/GalleryRenderer.tsx
@@ -1,4 +1,5 @@
-import { ImageListItem, ImageListItemBar, Skeleton, styled } from '@mui/material';
+import Masonry from '@mui/lab/Masonry';
+import { ImageList, ImageListItem, Skeleton, Theme, useMediaQuery } from '@mui/material';
 import { useMemo, useState } from 'react';
 
 import { Gallery, Picture } from 'types';
@@ -9,28 +10,18 @@ import PictureDialog from 'pages/GalleryDetails/components/PictureDialog';
 
 import Pagination from 'components/layout/Pagination';
 
-const ImageGrid = styled('div')(({ theme }) => ({
-  display: 'grid',
-  gridTemplateColumns: 'repeat(3, 1fr)',
-  gridAutoRows: 'minmax(200px, max-content)',
-  gridGap: theme.spacing(1),
-  [theme.breakpoints.down('lg')]: {
-    gridTemplateColumns: '1fr 1fr',
-  },
-  [theme.breakpoints.down('md')]: {
-    gridTemplateColumns: '1fr',
-  },
-}));
-
 export const GalleryRendererLoading = () => {
+  const mdDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
+  const lgUp = useMediaQuery((theme: Theme) => theme.breakpoints.up('lg'));
+
   return (
-    <ImageGrid>
+    <ImageList cols={mdDown ? 1 : lgUp ? 3 : 2} gap={8} variant='masonry'>
       {Array.from(Array(6)).map((_, i) => (
         <ImageListItem key={i} sx={{ border: 'none', width: '100%' }}>
           <Skeleton height={150} sx={{ borderRadius: (theme) => `${theme.shape.borderRadius}px` }} variant='rectangular' width='100%' />
         </ImageListItem>
       ))}
-    </ImageGrid>
+    </ImageList>
   );
 };
 
@@ -43,19 +34,20 @@ const GalleryRenderer = ({ id }: GalleryRendererProps) => {
   const pictures = useMemo(() => (data ? data.pages.map((page) => page.results).flat() : []), [data]);
   const [selectedImgId, setSelectedImgId] = useState<Picture['id'] | null>(null);
   const selectedImg = useMemo(() => pictures.find((picture) => picture.id === selectedImgId), [selectedImgId, pictures]);
+  const mdDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
+  const lgUp = useMediaQuery((theme: Theme) => theme.breakpoints.up('lg'));
 
   return (
     <>
-      <ImageGrid>
-        <Pagination fullWidth hasNextPage={hasNextPage} isLoading={isFetching} nextPage={() => fetchNextPage()}>
+      <Pagination fullWidth hasNextPage={hasNextPage} isLoading={isFetching} nextPage={() => fetchNextPage()}>
+        <Masonry columns={mdDown ? 1 : lgUp ? 3 : 2} spacing={1}>
           {pictures.map((image) => (
             <ImageListItem component='button' key={image.id} onClick={() => setSelectedImgId(image.id)} sx={{ cursor: 'pointer', border: 'none' }}>
               <img alt={image.image_alt} loading='lazy' src={image.image} />
-              {image.title && <ImageListItemBar subtitle={image.description} title={image.title} />}
             </ImageListItem>
           ))}
-        </Pagination>
-      </ImageGrid>
+        </Masonry>
+      </Pagination>
       {selectedImg && <PictureDialog galleryId={id} onClose={() => setSelectedImgId(null)} picture={selectedImg} />}
     </>
   );
