dcccee507609a899f73b81190630e94986c0db97
Martin Clementz <martincclementz@gmail.com>
2023-02-25 16:41:10 +0100

    feat(): update

--- src/pages/Landing/components/Wave.tsx
+++ src/pages/Landing/components/Wave.tsx
@@ -14,10 +14,10 @@ import { useAnalytics } from 'hooks/Utils';
 
 // import TihldeLogo from 'components/miscellaneous/TihldeLogo';
 
+import { ReactComponent as TihldeJubLogoSvg } from 'assets/icons/logo_jub.svg';
 import { ReactComponent as WaveBottom } from 'assets/img/waves/wave-bottom.svg';
 import { ReactComponent as WaveMid } from 'assets/img/waves/wave-mid.svg';
 import { ReactComponent as WaveTop } from 'assets/img/waves/wave-top.svg';
-import { ReactComponent as TihldeJubLogoSvg } from 'assets/icons/logo_jub.svg';
 
 const moveWave = keyframes`
   0% {
@@ -153,7 +153,9 @@ const Wave = () => {
     <div className={classes.root}>
       <div className={classes.container}>
         <div className={classes.content}>
-          <TihldeJubLogoSvg style={{ color: 'white', marginLeft: 'auto', marginRight: 'auto', display: 'block', height: 'auto', width: "70vw", maxWidth: 400 }} />
+          <TihldeJubLogoSvg
+            style={{ color: 'white', marginLeft: 'auto', marginRight: 'auto', display: 'block', height: 'auto', width: '70vw', maxWidth: 400 }}
+          />
           <Typography align='center' className={classes.contentText} variant='body1'>
             Linjeforeningen for Dataingeniør, Digital infrastruktur og cybersikkerhet, Digital forretningsutvikling, Digital samhandling og
             Informasjonsbehandling ved NTNU
