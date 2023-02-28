94e32b13ed9c0a3a159f82806437bbdc384cfd8f
Frikk Balder <33499052+MindChirp@users.noreply.github.com>
2023-02-28 16:46:50 +0100

    Feat/jubilee page button (#764)
    * Added button
    
    * Fixed main container height
    
    * Update Wave.tsx
    
    * Moved button closer to countdown
    
    ---------
    
    Co-authored-by: Martin Clementz <martcl@stud.ntnu.no>

--- src/pages/Landing/components/Wave.tsx
+++ src/pages/Landing/components/Wave.tsx
@@ -1,8 +1,9 @@
 import { keyframes } from '@emotion/react';
+import { OpenInNew } from '@mui/icons-material';
 import SignupIcon from '@mui/icons-material/AddRounded';
 import LoginIcon from '@mui/icons-material/LoginRounded';
 import ProfileIcon from '@mui/icons-material/PersonOutlineRounded';
-import { Button, Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import { differenceInDays } from 'date-fns';
 import { makeStyles } from 'makeStyles';
 import { Link } from 'react-router-dom';
@@ -39,14 +40,14 @@ const useStyles = makeStyles()((theme) => ({
     margin: 'unset',
     width: '100%',
     maxWidth: 'none',
-    height: 500,
+    height: 600,
   },
   container: {
     overflow: 'hidden',
     margin: 'auto',
     position: 'absolute',
     width: '100%',
-    height: 500,
+    height: 600,
     background: `linear-gradient(20deg, ${theme.palette.colors.gradient.main.bottom} 20%, ${theme.palette.colors.gradient.main.top} 80%)`,
   },
   content: {
@@ -199,6 +200,17 @@ const Wave = () => {
           <Typography color='white' fontSize={16} fontWeight={600} pt={3} textAlign='center'>
             {differenceInDays(new Date('2023-03-20'), new Date())} dager til!
           </Typography>
+          <Box
+            sx={{
+              width: 'fit-content',
+              margin: 'auto',
+              display: 'block',
+              marginTop: '10px',
+            }}>
+            <Button endIcon={<OpenInNew />} href='https://jubileum.tihlde.org/' variant='contained'>
+              Til Jubileumssiden
+            </Button>
+          </Box>
         </div>
         {/* Show snow if between November 15th and February 1st */}
         {(isAfterDateOfYear(10, 15) || isBeforeDateOfYear(1, 1)) && (
