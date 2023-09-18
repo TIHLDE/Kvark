83867ecde89099f14535fef9d586d4ca54fc8022
Martin Clementz <martcl@stud.ntnu.no>
2023-09-18 17:20:52 +0200

    chore: use normal tihlde LOGO

--- src/components/navigation/Topbar.tsx
+++ src/components/navigation/Topbar.tsx
@@ -7,10 +7,9 @@ import { Link } from 'react-router-dom';
 import URLS from 'URLS';
 
 //import TihldeLogo from 'components/miscellaneous/TihldeLogo';
+import TihldeLogo from 'components/miscellaneous/TihldeLogo';
 import { NavigationItem, NavigationOptions } from 'components/navigation/Navigation';
 import ProfileTopbarButton from 'components/navigation/ProfileTopbarButton';
-
-import { ReactComponent as TihldeJubLogoSvg } from 'assets/icons/logo_jub.svg';
 
 const useStyles = makeStyles()((theme) => ({
   appBar: {
@@ -195,7 +194,7 @@ const Topbar = ({ items, lightColor, darkColor, filledTopbar }: TopbarProps) => 
     return (
       <div className={classes.topbarMobile}>
         <Link aria-label='Til forsiden' to={URLS.landing}>
-          <TihldeJubLogoSvg style={{ color: 'white', margin: '3px', height: 40, width: 'auto', marginTop: 'auto', marginBottom: 'auto', display: 'block' }} />
+          <TihldeLogo className={classes.logo} darkColor={colorOnDark} lightColor={colorOnLight} size='large' />{' '}
         </Link>
         <ProfileTopbarButton darkColor={colorOnDark} lightColor={colorOnLight} />
       </div>
@@ -206,7 +205,7 @@ const Topbar = ({ items, lightColor, darkColor, filledTopbar }: TopbarProps) => 
       <AppBar className={cx(classes.appBar, isOnTop ? classes.fancyAppBar : classes.backdrop)} elevation={isOnTop ? 0 : 1} position='fixed'>
         <Toolbar className={classes.toolbar} disableGutters>
           <Link aria-label='Til forsiden' to={URLS.landing}>
-            <TihldeJubLogoSvg style={{ color: 'white', margin: '3px', height: 40, width: 'auto', marginTop: 'auto', marginBottom: 'auto', display: 'block' }} />
+            <TihldeLogo className={classes.logo} darkColor={colorOnDark} lightColor={colorOnLight} size='large' />{' '}
           </Link>
           <div className={cx(classes.items, (theme.palette.mode === 'light' ? colorOnLight : colorOnDark) !== 'white' && !filledTopbar && classes.black)}>
             {items.map((item, i) => (
