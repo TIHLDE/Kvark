8b38c25bd4d96a5c2e1e41b95ee1c536ea0cb684
Mads Nylund <madsnyl@gmail.com>
2023-10-11 21:03:32 +0200

    added error for invalid url, and download option

--- src/hooks/QRCode.tsx
+++ src/hooks/QRCode.tsx
@@ -1,30 +1,29 @@
 import { useMutation, UseMutationResult, useQuery, useQueryClient } from 'react-query';
 
-import { RequestResponse, QRCode } from 'types';
+import { QRCode, RequestResponse } from 'types';
 
 import API from 'api/api';
-
 
 export const QR_CODE_QUERY_KEY = 'qr-code';
 
 export const useQRCodes = () => {
-    return useQuery<Array<QRCode>, RequestResponse>([QR_CODE_QUERY_KEY], () => API.getQRCodes());
-}
+  return useQuery<Array<QRCode>, RequestResponse>([QR_CODE_QUERY_KEY], () => API.getQRCodes());
+};
 
 export const useCreateQRCode = (): UseMutationResult<QRCode, RequestResponse, QRCode, unknown> => {
-    const queryClient = useQueryClient();
-    return useMutation((item) => API.createQRCode(item), {
-        onSuccess: () => {
-            queryClient.invalidateQueries(QR_CODE_QUERY_KEY);
-        },
-    });
-}
+  const queryClient = useQueryClient();
+  return useMutation((item) => API.createQRCode(item), {
+    onSuccess: () => {
+      queryClient.invalidateQueries(QR_CODE_QUERY_KEY);
+    },
+  });
+};
 
-export const useDeleteQRCode = (): UseMutationResult<RequestResponse, RequestResponse, number, unknown> => {
-    const queryClient = useQueryClient();
-    return useMutation((id) => API.deleteQRCode(id), {
-        onSuccess: () => {
-            queryClient.invalidateQueries(QR_CODE_QUERY_KEY);
-        },
-    });
-}
+export const useDeleteQRCode = (qrCodeId: QRCode['id']): UseMutationResult<RequestResponse, RequestResponse, unknown, unknown> => {
+  const queryClient = useQueryClient();
+  return useMutation(() => API.deleteQRCode(qrCodeId), {
+    onSuccess: () => {
+      queryClient.invalidateQueries(QR_CODE_QUERY_KEY);
+    },
+  });
+};
