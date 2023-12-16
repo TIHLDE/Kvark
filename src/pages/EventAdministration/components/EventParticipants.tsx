d7a6d175b781a6d73487b5b0a6dc9bc9231d05e4
Mads Nylund <73914541+MadsNyl@users.noreply.github.com>
2023-12-16 12:30:21 +0100

    added id for each user in copy of participants of an event (#940)

--- src/pages/EventAdministration/components/EventParticipants.tsx
+++ src/pages/EventAdministration/components/EventParticipants.tsx
@@ -54,6 +54,7 @@ const Registrations = ({ onWait = false, eventId }: RegistrationsProps) => {
     return registrations
       .map((registration) => {
         const data: string[] = [];
+        data.push(registration.user_info.user_id);
         names && data.push(`${registration.user_info.first_name} ${registration.user_info.last_name}`);
         emails && data.push(registration.user_info.email);
         return data.join(',');
