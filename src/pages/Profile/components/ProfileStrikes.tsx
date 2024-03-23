ae9beafc7bb6a4406d23532f91a19fc47eb1a276
Mads Nylund <madsnyl@gmail.com>
2024-03-23 21:09:20 +0100

    started on strike listing

--- src/pages/Profile/components/ProfileStrikes.tsx
+++ src/pages/Profile/components/ProfileStrikes.tsx
@@ -1,34 +1,35 @@
-import { Stack, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import URLS from 'URLS';
 
 import { useUser, useUserStrikes } from 'hooks/User';
 
-import Paper from 'components/layout/Paper';
 import NotFoundIndicator from 'components/miscellaneous/NotFoundIndicator';
 import StrikeListItem from 'components/miscellaneous/StrikeListItem';
+import { Card, CardContent } from 'components/ui/card';
 
 function ProfileStrikes() {
   const { data = [] } = useUserStrikes();
   const { data: user } = useUser();
   return (
-    <Stack gap={1}>
-      <Paper>
-        <Typography>
-          Informasjon om prikksystemet finner du i <Link to={URLS.eventRules}>arrangementsreglene</Link>.{' '}
-          {Boolean(data.length) && (
-            <span>
-              Uenig i en prikk? Send epost til <a href='mailto:naeringslivsminister@tihlde.org'>naeringslivsminister@tihlde.org</a>.
-            </span>
-          )}
-        </Typography>
-      </Paper>
+    <div className='space-y-2'>
+      <Card>
+        <CardContent className='py-4'>
+          <h1>
+            Informasjon om prikksystemet finner du i <Link to={URLS.eventRules}>arrangementsreglene</Link>.{' '}
+            {Boolean(data.length) && (
+              <span>
+                Uenig i en prikk? Send epost til <a href='mailto:naeringslivsminister@tihlde.org'>naeringslivsminister@tihlde.org</a>.
+              </span>
+            )}
+          </h1>
+        </CardContent>
+      </Card>
       {data.length ? (
         user && data.map((strike) => <StrikeListItem key={strike.id} strike={strike} user={user} />)
       ) : (
         <NotFoundIndicator header='Fant ingen prikker' subtitle='Du har ingen prikker!' />
       )}
-    </Stack>
+    </div>
   );
 }
 
