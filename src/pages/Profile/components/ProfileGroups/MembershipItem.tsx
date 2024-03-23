698f2f9297d4708eb99613b4c4b945aee2e6d282
Mads Nylund <madsnyl@gmail.com>
2024-03-23 21:04:09 +0100

    refactored membershippage

--- src/pages/Profile/components/ProfileGroups/MembershipItem.tsx
+++ src/pages/Profile/components/ProfileGroups/MembershipItem.tsx
@@ -1,4 +1,4 @@
-import { Box, ButtonBase, Divider, Skeleton, Stack, Typography } from '@mui/material';
+import { ButtonBase, Divider, Skeleton, Stack } from '@mui/material';
 import { parseISO } from 'date-fns';
 import { Link } from 'react-router-dom';
 import URLS from 'URLS';
@@ -7,41 +7,31 @@ import { formatDate, getMembershipType } from 'utils';
 import { Membership } from 'types';
 
 import Paper from 'components/layout/Paper';
-import AspectRatioImg, { AspectRatioLoading } from 'components/miscellaneous/AspectRatioImg';
+import { AspectRatioLoading } from 'components/miscellaneous/AspectRatioImg';
+import { Card, CardContent } from 'components/ui/card';
+import { Separator } from 'components/ui/separator';
+
+import TIHLDE_LOGO from 'assets/img/TihldeBackground.jpg';
 
 export type MembershipItemProps = {
   membership: Membership;
 };
 
 const MembershipItem = ({ membership }: MembershipItemProps) => (
-  <Paper noOverflow noPadding>
-    <ButtonBase
-      component={Link}
-      focusRipple
-      sx={{ width: '100%', height: '100%', minHeight: 90, justifyContent: 'flex-start' }}
-      to={URLS.groups.details(membership.group.slug)}>
-      <Stack
-        alignItems='center'
-        direction='row'
-        divider={<Divider flexItem orientation='vertical' />}
-        gap={1}
-        justifyContent='flex-start'
-        sx={{ pl: 1, overflow: 'hidden' }}>
-        {/* TODO: fjern div rundt AspectRatioImg når flere nettlesere støtter aspect-ratio i css - https://caniuse.com/mdn-css_properties_aspect-ratio */}
-        <Box sx={{ display: 'block', height: 70, width: 70 }}>
-          <AspectRatioImg alt={membership.group.image_alt || ''} borderRadius ratio={1} src={membership.group.image || ''} sx={{ width: 70, height: 70 }} />
-        </Box>
-        <Stack>
-          <Typography sx={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }} variant='h3'>
-            {membership.group.name}
-          </Typography>
-          <Typography sx={{ fontSize: '0.8rem' }}>
+  <Card className='hover:bg-secondary'>
+    <CardContent className='p-0'>
+      <Link className='px-3 py-2 flex items-center space-x-4' to={URLS.groups.details(membership.group.slug)}>
+        <img alt={membership.group.image_alt || ''} className='w-16 h-16 rounded-sm object-cover' src={membership.group.image || TIHLDE_LOGO} />
+        <Separator className='h-16' orientation='vertical' />
+        <div className='space-y-1'>
+          <h1 className='text-lg break-words font-semibold'>{membership.group.name}</h1>
+          <h1 className='text-sm'>
             {`${formatDate(parseISO(membership.created_at), { time: false, fullMonth: true })} -> nå - ${getMembershipType(membership.membership_type)}`}
-          </Typography>
-        </Stack>
-      </Stack>
-    </ButtonBase>
-  </Paper>
+          </h1>
+        </div>
+      </Link>
+    </CardContent>
+  </Card>
 );
 
 export default MembershipItem;
