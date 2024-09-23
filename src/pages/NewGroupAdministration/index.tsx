31911d077b7ca535a76047c9c017455defc2c4d4
Petra Halvorsen <petrahalvorsen@dhcp-10-22-175-18.wlan.ntnu.no>
2024-09-23 16:31:02 +0200

    fix

--- src/pages/NewGroupAdministration/index.tsx
+++ src/pages/NewGroupAdministration/index.tsx
@@ -1,51 +1,47 @@
+import { useForm } from 'react-hook-form';
 
-
-import { Input } from "components/ui/input";
-import { Select, SelectItem, SelectTrigger, SelectContent } from "components/ui/select";
-import { FormLabel,  FormItem, Form,  } from "components/ui/form";
-import { useForm } from "react-hook-form";
-import { Button } from "components/ui/button";
+import { Button } from 'components/ui/button';
+import { Form, FormItem, FormLabel } from 'components/ui/form';
+import { Input } from 'components/ui/input';
+import { Select, SelectContent, SelectItem, SelectTrigger } from 'components/ui/select';
 
 export default function NewGroupAdministration() {
   const form = useForm();
 
-  return <div className="max-w-5xl mx-auto pt-24 flex flex-col gap-2 px-2">
-    <h1 className="text-4xl font-bold">Opprett ny gruppe</h1>
-    <p className="text-muted-foreground mb-8">Velg gruppetype og opprett en ny gruppe.</p>
-    <Form {...form}>
-
-    <form onSubmit={()=>{}}>
-    <FormItem className="mb-5">
-        <FormLabel>Gruppenavn</FormLabel>
-            <Input placeholder="Gruppenavn" />
-    </FormItem>
-    <FormItem style={{ marginBottom: '20px' }}>
-        <FormLabel>Slug</FormLabel>
-            <Input placeholder="Slug" />
-            <p className="text-muted-foreground mb-8 text-sm">
+  return (
+    <div className='max-w-5xl mx-auto pt-24 flex flex-col gap-2 px-2'>
+      <h1 className='text-4xl font-bold'>Opprett ny gruppe</h1>
+      <p className='text-muted-foreground mb-8'>Velg gruppetype og opprett en ny gruppe.</p>
+      <Form {...form}>
+        <form onSubmit={() => {}}>
+          <FormItem className='mb-5'>
+            <FormLabel>Gruppenavn</FormLabel>
+            <Input placeholder='Gruppenavn' />
+          </FormItem>
+          <FormItem style={{ marginBottom: '20px' }}>
+            <FormLabel>Slug</FormLabel>
+            <Input placeholder='Slug' />
+            <p className='text-muted-foreground mb-8 text-sm'>
               En slug er en kort tekststreng som brukes i URL-adresser for å identifisere en spesifikk ressurs på en webside
-              </p>
-    </FormItem>
-    <FormItem className = "mb-5">
-      <FormLabel>Gruppetype</FormLabel>
-        <Select>
-          <SelectTrigger className="text-muted-foreground">Velg gruppetype...</SelectTrigger>
-          <SelectContent>
-      <SelectItem value="board">Styre</SelectItem>
-      <SelectItem value="subGroup">Undergruppe</SelectItem>
-      <SelectItem value="committee">Komité</SelectItem>
-      <SelectItem value="interestGroup">Interesse Gruppe</SelectItem>
-      </SelectContent>
-    </Select>
-    </FormItem>
-    <FormItem className="mb-5">
-      <Button
-        variant="default"
-      >
-        Opprett Gruppe
-      </Button>
-    </FormItem>
-    </form>
-    </Form>
-  </div>;
+            </p>
+          </FormItem>
+          <FormItem className='mb-5'>
+            <FormLabel>Gruppetype</FormLabel>
+            <Select>
+              <SelectTrigger className='text-muted-foreground'>Velg gruppetype...</SelectTrigger>
+              <SelectContent>
+                <SelectItem value='board'>Styre</SelectItem>
+                <SelectItem value='subGroup'>Undergruppe</SelectItem>
+                <SelectItem value='committee'>Komité</SelectItem>
+                <SelectItem value='interestGroup'>Interesse Gruppe</SelectItem>
+              </SelectContent>
+            </Select>
+          </FormItem>
+          <FormItem className='mb-5'>
+            <Button variant='default'>Opprett Gruppe</Button>
+          </FormItem>
+        </form>
+      </Form>
+    </div>
+  );
 }
