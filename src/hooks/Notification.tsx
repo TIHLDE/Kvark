a1df78e9127e28c353541ecd515fbb662e51bbdb
Aleksander Evensen <ahse03@gmail.com>
2025-09-12 02:40:08 +0200

    Fixed notifications crashing site

--- src/hooks/Notification.tsx
+++ src/hooks/Notification.tsx
@@ -1,47 +1,31 @@
-import { useInfiniteQuery, useQueryClient } from '@tanstack/react-query';
+import { infiniteQueryOptions, useMutation, useQueryClient } from '@tanstack/react-query';
 import API from '~/api/api';
-import { RequestResponse } from '~/types';
-import { useEffect } from 'react';
 
 export const NOTIFICATION_QUERY_KEY = 'notification';
 
-type GetNotificationData = Awaited<ReturnType<typeof API.getNotifications>>;
+export const notificationQueryOptions = infiniteQueryOptions({
+  queryKey: [NOTIFICATION_QUERY_KEY],
+  queryFn: ({ pageParam }) => API.getNotifications({ page: pageParam }),
+  initialPageParam: 1,
+  getNextPageParam: (lastPage) => lastPage.next,
+});
 
-export const useNotifications = (modalOpen: boolean) => {
+export const useMarkNotificationAsRead = () => {
   const queryClient = useQueryClient();
-
-  const query = useInfiniteQuery<GetNotificationData, RequestResponse>({
-    queryKey: [NOTIFICATION_QUERY_KEY],
-    queryFn: ({ pageParam }) => API.getNotifications({ page: pageParam }),
-    initialPageParam: 1,
-    getNextPageParam: (lastPage) => lastPage.next,
+  return useMutation({
+    mutationFn: ({ id, read }: { id: number; read: boolean }) => API.updateNotification(id, { read }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [NOTIFICATION_QUERY_KEY] });
+    },
   });
+};
 
-  useEffect(() => {
-    if (!modalOpen) {
-      return;
-    }
-    if (!query.data || !Array.isArray(query.data.pages)) {
-      return;
-    }
-
-    const currentData = query.data;
-    const unread = query.data.pages.flatMap((page) => page.results).filter(({ read }) => !read);
-    if (unread.length) {
-      // TODO: This should be a single API call to mark multiplle notifications as read
-      // TODO: Need backend chaneges
-      Promise.allSettled(unread.map(({ id }) => API.updateNotification(id, { read: true }))).then((results) => {
-        const successfullyUpdatedIds = new Set(results.map((result, index) => (result.status === 'fulfilled' ? unread[index].id : null)).filter(Boolean));
-
-        // Once all notifications are marked as read update the query data
-        const newQueryData = currentData.pages.map((page) => ({
-          ...page,
-          results: page.results.map((v) => (successfullyUpdatedIds.has(v.id) ? { ...v, read: true } : v)),
-        }));
-        queryClient.setQueryData([NOTIFICATION_QUERY_KEY], newQueryData);
-      });
-    }
-  }, [modalOpen, query.data]);
-
-  return query;
+export const useMarkAllNotificationsAsRead = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: ({ ids, read = true }: { ids: number[]; read?: boolean }) => Promise.allSettled(ids.map((id) => API.updateNotification(id, { read }))),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [NOTIFICATION_QUERY_KEY] });
+    },
+  });
 };
