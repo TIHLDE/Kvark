cd91ae31c23ee6333b202d605d17ee332f02b2a9
Aleksander Evensen <ahse03@gmail.com>
2025-09-16 02:19:59 +0200

    Adding sourcemaps might actually work

--- post-build.sh
+++ post-build.sh
@@ -1,4 +1,4 @@
 if [ -n "$POSTHOG_CLI_ENV_ID" ] && [ -n "$POSTHOG_CLI_TOKEN" ]; then
-  pnpm posthog-cli sourcemap inject --directory ./build
-  pnpm posthog-cli upload --directory ./build
+  pnpm posthog-cli --host https://eu.posthog.com sourcemap inject --directory ./build
+  pnpm posthog-cli --host https://eu.posthog.com sourcemap upload --directory ./build
 fi
