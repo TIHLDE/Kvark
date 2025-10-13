84a24c69b4d77e38f86327fc698c621b3f982a0c
Aleksander Evensen <ahse03@gmail.com>
2025-10-14 00:09:42 +0200

    Format, lint fix

--- src/pages/Profile/components/ProfileSettings/UserSettings.tsx
+++ src/pages/Profile/components/ProfileSettings/UserSettings.tsx
@@ -108,7 +108,8 @@ export const UserSettings = ({ isAdmin, user }: UserSettingsProps) => {
         <FormTextarea
           description={
             <>
-              <span className='font-bold'>Ikke</span> skriv tulleallergier, eller &quot;ingen&quot;pn. Dine allergier vises til bedrifter/arrangører ved arrangementer
+              <span className='font-bold'>Ikke</span> skriv tulleallergier, eller &quot;ingen&quot;pn. Dine allergier vises til bedrifter/arrangører ved
+              arrangementer
             </>
           }
           form={form}
