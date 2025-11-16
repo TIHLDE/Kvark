25b3e380b720326934c3f4e0f89bf0fc65181841
Aleksander Evensen <ahse03@gmail.com>
2025-11-16 19:16:31 +0100

    Fixed to use .vercel folder as output

--- post-build.sh
+++ post-build.sh
@@ -1,4 +1,4 @@
 if [ -n "$POSTHOG_CLI_ENV_ID" ] && [ -n "$POSTHOG_CLI_TOKEN" ]; then
-  pnpm dlx @posthog/cli --host https://eu.posthog.com sourcemap inject --directory ./.output
-  pnpm dlx @posthog/cli --host https://eu.posthog.com sourcemap upload --directory ./.output
+  pnpm dlx @posthog/cli --host https://eu.posthog.com sourcemap inject --directory ./.vercel
+  pnpm dlx @posthog/cli --host https://eu.posthog.com sourcemap upload --directory ./.vercel
 fi
